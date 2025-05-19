cb98517e1fcb15b4a5b5010c7272d6fcba535a02
BangJeongBin <bangjungbin@gmail.com>
2025-05-19 11:20:38 +0900

    Baekjoon_25083

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,24 @@ public class Main {
     public static void solution() throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        bw.write("|\\_/|");
+        bw.write("         ,r'\"7");
         bw.newLine(); // 줄 바꿈
 
-        bw.write("|q p|   /}");
+        bw.write("r`-_   ,'  ,/");
         bw.newLine(); // 줄 바꿈
 
-        bw.write("( 0 )\"\"\"\\");
+        bw.write(" \\. \". L_r'");
         bw.newLine(); // 줄 바꿈
 
-        bw.write("|\"^\"`    |");
+        bw.write("   `~\\/");
         bw.newLine(); // 줄 바꿈
 
-        bw.write("||_/=\\\\__|");
+        bw.write("      |");
         bw.newLine(); // 줄 바꿈
+
+        bw.write("      |");
+        bw.newLine(); // 줄 바꿈
+
 
         bw.flush();
         bw.close();
