956c30daa62d097538893c54cc83c663086b3a1b
BangJeongBin <bangjungbin@gmail.com>
2025-05-21 21:24:28 +0900

    Baekjoon_10926

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,9 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        String name = br.readLine();
 
-        Long a = Long.parseLong(st.nextToken()); // int는 10의 9승까지 표현 가능, Long은 10의 18승 까지 표현 가능.
-        Long b = Long.parseLong(st.nextToken()); // 백준 입력값은 10의 12승까지 입력하기 때문에 int형으로 제출 시 NumberFormatException에 걸림
-        Long c = Long.parseLong(st.nextToken());
-
-        bw.write(String.valueOf(a + b + c));
+        bw.write(name + "??!");
 
         bw.flush();
         bw.close();
