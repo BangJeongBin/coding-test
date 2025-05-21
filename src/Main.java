d6bd3c31228cb3327a4b54606a3194784c695ed0
BangJeongBin <bangjungbin@gmail.com>
2025-05-21 21:17:00 +0900

    Baekjoon_11382

--- src/Main.java
+++ src/Main.java
@@ -8,26 +8,15 @@ public class Main {
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
+        Long a = Long.parseLong(st.nextToken()); // int는 10의 9승까지 표현 가능, Long은 10의 18승 까지 표현 가능.
+        Long b = Long.parseLong(st.nextToken()); // 백준 입력값은 10의 12승까지 입력하기 때문에 int형으로 제출 시 NumberFormatException에 걸림
+        Long c = Long.parseLong(st.nextToken());
 
-        bw.write(String.valueOf(a + b));
-        bw.newLine();
-
-        bw.write(String.valueOf(a - b));
-        bw.newLine();
-
-        bw.write(String.valueOf(a * b));
-        bw.newLine();
-
-        bw.write(String.valueOf(a / b));
-        bw.newLine();
-
-        bw.write(String.valueOf(a % b));
-        bw.newLine();
+        bw.write(String.valueOf(a + b + c));
 
         bw.flush();
         bw.close();
+        br.close();
     }
 
     public static void main(String[] args) throws IOException {
