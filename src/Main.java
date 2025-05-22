e965c5eea06ae91af43ecbd6b5d377d44cea84cf
BangJeongBin <bangjungbin@gmail.com>
2025-05-22 16:37:41 +0900

    Baekjoon_10952

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,19 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int num = Integer.parseInt(br.readLine());
-
-        for (int i = 1; i <= num; i++) {
+        while (true) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
+            if (a == 0 && b == 0) {
+                break;
+            }
             bw.write(String.valueOf(a + b) + "\n");
         }
 
