3158bcef9518b3b46b72d59f3f392eb9b1e179b7
BangJeongBin <bangjungbin@gmail.com>
2025-05-22 15:21:23 +0900

    Baekjoon_2753

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,16 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int year = Integer.parseInt(br.readLine());
 
-        int point = Integer.parseInt(st.nextToken());
-
-        if (90 <= point) {
-            bw.write("A");
-        } else if (80 <= point) {
-            bw.write("B");
-        } else if (70 <= point) {
-            bw.write("C");
-        } else if (60 <= point) {
-            bw.write("D");
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            bw.write("1");
         } else {
-            bw.write("F");
+            bw.write("0");
         }
 
         bw.flush();
