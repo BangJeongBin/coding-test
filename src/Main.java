a164cf07788d08768c20088eb1aa06feffe22faf
BangJeongBin <bangjungbin@gmail.com>
2025-05-22 17:13:09 +0900

    Baekjoon_15552

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,20 @@ import java.util.*;
 public class Main {
     public static void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-        String str;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        while ((str = br.readLine()) != null) {
-            st = new StringTokenizer(str, " ");
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= num; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            sb.append(a + b).append("\n");
+            bw.write(String.valueOf(a + b) + "\n");
         }
-        System.out.print(sb);
 
+        bw.flush();
+        bw.close();
         br.close();
     }
 
