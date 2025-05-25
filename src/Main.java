830a7e46eac117790c81d56404007385444b3ef1
BangJeongBin <bangjungbin@gmail.com>
2025-05-25 21:12:13 +0900

    Baekjoon_10807

--- src/Main.java
+++ src/Main.java
@@ -6,25 +6,28 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer st1 = new StringTokenizer(br.readLine(), " "); // n, x 값을 받기 위한 입력
-        StringTokenizer st2 = new StringTokenizer(br.readLine(), " "); // n개의 배열의 값을 받기 위한 입력
+        StringTokenizer st1 = new StringTokenizer(br.readLine(), " "); // 입력받을 정수의 개수
+        StringTokenizer st2 = new StringTokenizer(br.readLine(), " "); // n개의 정수들
+        StringTokenizer st3 = new StringTokenizer(br.readLine(), " "); // 찾을려는 정수
 
-        int n = Integer.parseInt(st1.nextToken()); // 배열의 length
-        int x = Integer.parseInt(st1.nextToken()); // 배열에서 비교연산을 하기 위한 기준값
-        int[] a = new int[n]; // st2로 입력받는 배열 초기화
+        int n = Integer.parseInt(st1.nextToken());
+        int v = Integer.parseInt(st3.nextToken());
+        int k = 0; // count 값 저장 변수
+
+        int[] a = new int[n]; // n개의 정수를 받을 배열 초기화
 
         int i = 0; // while 증감값
-
         while (st2.hasMoreTokens()) {
             a[i] = Integer.parseInt(st2.nextToken());
             i++;
         }
 
         for (int j = 0; j < a.length; j++) {
-            if (a[j] < x) {
-                bw.write(a[j] + " ");
+            if (a[j] == v) {
+                k++;
             }
         }
+        bw.write(String.valueOf(k));
 
         bw.flush();
         bw.close();
