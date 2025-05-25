0c51e7071b53dc049248ba6b719bc59c84bb4927
BangJeongBin <bangjungbin@gmail.com>
2025-05-25 20:54:44 +0900

    Baekjoon_10871

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,24 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int num = Integer.parseInt(br.readLine());
+        StringTokenizer st1 = new StringTokenizer(br.readLine(), " "); // n, x 값을 받기 위한 입력
+        StringTokenizer st2 = new StringTokenizer(br.readLine(), " "); // n개의 배열의 값을 받기 위한 입력
 
-        for (int i = 1; i <= num; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st1.nextToken()); // 배열의 length
+        int x = Integer.parseInt(st1.nextToken()); // 배열에서 비교연산을 하기 위한 기준값
+        int[] a = new int[n]; // st2로 입력받는 배열 초기화
 
-            bw.write(String.valueOf(a + b) + "\n");
+        int i = 0; // while 증감값
+
+        while (st2.hasMoreTokens()) {
+            a[i] = Integer.parseInt(st2.nextToken());
+            i++;
+        }
+
+        for (int j = 0; j < a.length; j++) {
+            if (a[j] < x) {
+                bw.write(a[j] + " ");
+            }
         }
 
         bw.flush();
