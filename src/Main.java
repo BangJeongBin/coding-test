0ac149a150865a7911047d22b8347fa899a01fb7
BangJeongBin <bangjungbin@gmail.com>
2025-05-26 16:13:55 +0900

    Baekjoon_5597

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,22 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer st1 = new StringTokenizer(br.readLine(), " "); // 입력받을 정수의 개수
-        StringTokenizer st2 = new StringTokenizer(br.readLine(), " "); // n개의 정수들
-        StringTokenizer st3 = new StringTokenizer(br.readLine(), " "); // 찾을려는 정수
+        int[] studentNo = new int[31]; // 출력 시 index의 값을 쓰는 것이 아닌 index 번호 자체를 쓰기 때문에 31까지 사용
 
-        int n = Integer.parseInt(st1.nextToken());
-        int v = Integer.parseInt(st3.nextToken());
-        int k = 0; // count 값 저장 변수
-
-        int[] a = new int[n]; // n개의 정수를 받을 배열 초기화
-
-        int i = 0; // while 증감값
-        while (st2.hasMoreTokens()) {
-            a[i] = Integer.parseInt(st2.nextToken());
-            i++;
+        for (int i = 0; i < 28; i++) {
+            int a = Integer.parseInt(br.readLine());
+            studentNo[a] = 1;
         }
 
-        for (int j = 0; j < a.length; j++) {
-            if (a[j] == v) {
-                k++;
+        for (int j = 1; j < studentNo.length; j++) {
+            if (studentNo[j] != 1) {
+                bw.write(String.valueOf(j) + "\n");
             }
         }
-        bw.write(String.valueOf(k));
 
         bw.flush();
         bw.close();
