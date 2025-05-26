9ab1bf6e72aa00593aa0f0e589b3c6d1cc5c925e
BangJeongBin <bangjungbin@gmail.com>
2025-05-26 16:16:07 +0900

    Baekjoon_2738

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,30 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void solution() throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int[] studentNo = new int[31]; // 출력 시 index의 값을 쓰는 것이 아닌 index 번호 자체를 쓰기 때문에 31까지 사용
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n][m];
 
-        for (int i = 0; i < 28; i++) {
-            int a = Integer.parseInt(br.readLine());
-            studentNo[a] = 1;
-        }
-
-        for (int j = 1; j < studentNo.length; j++) {
-            if (studentNo[j] != 1) {
-                bw.write(String.valueOf(j) + "\n");
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+        StringBuilder sb = new StringBuilder();
 
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-
-    public static void main(String[] args) throws IOException {
-        solution();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                sb.append(arr[i][j] + Integer.parseInt(st.nextToken()) + " ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
     }
 }
