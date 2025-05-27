9ce3c981b1a6897f46897cb46172bcd3a6b93c68
BangJeongBin <bangjungbin@gmail.com>
2025-05-27 20:14:00 +0900

    Baekjoon_2744

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,20 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String askii = br.readLine();
+        String alphabet = br.readLine();
+        String answer = "";
 
-        bw.write(String.valueOf(askii.length()));
+        for (int i = 0; i < alphabet.length(); i++) {
+            char spell = alphabet.charAt(i);
+
+            if (Character.isUpperCase(spell)) {
+                answer += Character.toLowerCase(spell);
+            } else {
+                answer += Character.toUpperCase(spell);
+            }
+        }
+
+        bw.write(answer);
 
         bw.flush();
         bw.close();
@@ -18,3 +29,4 @@ public class Main {
         solution();
     }
 }
+
