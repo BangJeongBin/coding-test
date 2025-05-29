141199e8f768964a86300079588777f13bdb0c6f
BangJeongBin <bangjungbin@gmail.com>
2025-05-29 14:10:45 +0900

    Baekjoon_9086

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,16 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String str;
+        int n = Integer.parseInt(br.readLine());
 
-        while ((str = br.readLine()) != null) {
-            System.out.println(str);
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            int a =  str.length();
+
+            System.out.println(String.valueOf(str.charAt(0)) + str.charAt(a - 1)); // 아스키 코드값끼리 정수덧셈을 실행해서 강제적으로 문자열합을 유도함
+            //System.out.println("" + str.charAt(0) + str.charAt(a - 1));
         }
+
 
         bw.flush();
         bw.close();
