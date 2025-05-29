2824c4274b4d64a3b8c7e0f0ce34bef5ded82909
BangJeongBin <bangjungbin@gmail.com>
2025-05-29 13:56:12 +0900

    Baekjoon_11718

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,11 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String word = br.readLine(); // 첫번쨰 입력받을 단어
-        int i = Integer.parseInt(br.readLine()); // 두번째 입력받는 index 번호
-        char a = word.charAt(--i);
+        String str;
 
-        bw.write(a);
+        while ((str = br.readLine()) != null) {
+            System.out.println(str);
+        }
 
         bw.flush();
         bw.close();
