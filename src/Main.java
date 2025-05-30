57abb99e7773232cfde0a031fd65cf9bdff3d252
BangJeongBin <bangjungbin@gmail.com>
2025-05-30 22:10:31 +0900

    Baekjoon_2475

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,28 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
+    public static int square(int nn) { // 파라메터를 제곱해서 리턴하는 메서드
+        return nn * nn;
+    }
+
     public static void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        Long a = Long.parseLong(st.nextToken());
-        Long b = Long.parseLong(st.nextToken());
+        int n = 0;
 
-        Long answer = (a + b) * (a - b);
-
-        bw.write(String.valueOf(answer));
+        while (st.hasMoreTokens()) {
+            n += square(Integer.parseInt(st.nextToken()));
+        }
+        bw.write(String.valueOf(n % 10));
 
         bw.flush();
         bw.close();
         br.close();
     }
+
 
     public static void main(String[] args) throws IOException {
         solution();
