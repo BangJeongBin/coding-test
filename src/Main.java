ab07b06edc0846bf12e90c0bf4ae9547bbf2c681
BangJeongBin <bangjungbin@gmail.com>
2025-05-30 21:54:57 +0900

    Baekjoon_15964

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,18 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(br.readLine());
+        Long a = Long.parseLong(st.nextToken());
+        Long b = Long.parseLong(st.nextToken());
 
-        for (int i = 0; i < n; i++) {
-            String str = br.readLine();
-            int a =  str.length();
+        Long answer = (a + b) * (a - b);
 
-            System.out.println(String.valueOf(str.charAt(0)) + str.charAt(a - 1)); // 아스키 코드값끼리 정수덧셈을 실행해서 강제적으로 문자열합을 유도함
-            //System.out.println("" + str.charAt(0) + str.charAt(a - 1));
-        }
-
+        bw.write(String.valueOf(answer));
 
         bw.flush();
         bw.close();
