54f52da55d9b2d7d165dfc5f7a6e88cd4798057e
BangJeongBin <bangjungbin@gmail.com>
2025-05-31 23:38:43 +0900

    Baekjoon_2338

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,17 @@
 import java.io.*;
-import java.util.*;
+import java.math.BigInteger;
 
 public class Main {
-
-    public static int square(int nn) { // 파라메터를 제곱해서 리턴하는 메서드
-        return nn * nn;
-    }
 
     public static void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = 0;
-
-        while (st.hasMoreTokens()) {
-            n += square(Integer.parseInt(st.nextToken()));
-        }
-        bw.write(String.valueOf(n % 10));
+        BigInteger a = new BigInteger(br.readLine()); // "10진수로 1,000자리를 넘지 않으며 양수와 음수가 모두 주어질 수 있다." 가 문제의 조건이므로
+        BigInteger b = new BigInteger(br.readLine()); // 일반적인 Long이 아닌 math 클래스를 사용한다.
+        bw.write(String.valueOf(a.add(b)) + "\n");
+        bw.write(String.valueOf(a.subtract(b)) + "\n");
+        bw.write(String.valueOf(a.multiply(b)) + "\n");
 
         bw.flush();
         bw.close();
