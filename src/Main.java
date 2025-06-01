bb9a472a282b16f4bca83d50191f309679540abc
BangJeongBin <bangjungbin@gmail.com>
2025-06-01 21:51:38 +0900

    Baekjoon_4153

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
@@ -6,21 +7,27 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                int a = n - i;
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int z = Integer.parseInt(st.nextToken());
 
-                if (a < j) {
-                    bw.write("*");
-                } else {
-                    bw.write(" ");
-                }
+
+            // 0 0 0 을 입력받으면 종료
+            if (x == 0 && y == 0 && z == 0) break;
+
+            if ((x * x + y * y) == z * z) {
+                System.out.println("right");
+            } else if (x * x == (y * y + z * z)) {
+                System.out.println("right");
+            } else if (y * y == (z * z + x * x)) {
+                System.out.println("right");
+            } else {
+                System.out.println("wrong");
             }
-            bw.write("\n");
         }
-
         bw.flush();
         bw.close();
         br.close();
