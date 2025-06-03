b205d748aa16458e35efb2c87418d880409c4fbe
BangJeongBin <bangjungbin@gmail.com>
2025-06-03 18:01:04 +0900

    Baekjoon_31403

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,14 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
-        String s = br.readLine();
-        int answer = 0;
+        String a = br.readLine();
+        String b = br.readLine();
+        String c = br.readLine();
 
-        for (int i = 0; i < n; i++) {
-             //answer += Character.getNumericValue(s.charAt(i)); // char형(ascii)을 int형으로 바꿔주는 정적메서드
-             answer += s.charAt(i) - '0'; // 이 방법이 범용적으로 많이 쓰임. 안정성은 위에 코드가 더 좋음
-        }
-
-        bw.write(String.valueOf(answer));
+        bw.write(String.valueOf(Integer.parseInt(a) + Integer.parseInt(b) - Integer.parseInt(c)));
+        bw.write("\n");
+        String s = a + b;
+        bw.write(String.valueOf(Integer.parseInt(s) - Integer.parseInt(c)));
 
         bw.flush();
         bw.close();
