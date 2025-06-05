0fed98956eff22f6202073561523d72a2ba0a5c8
BangJeongBin <bangjungbin@gmail.com>
2025-06-05 18:17:37 +0900

    Baekjoon_2577

--- src/Main.java
+++ src/Main.java
@@ -7,23 +7,26 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine()); // 정수의 개수
-        int maxNum = -1000000;
-        int minNum = 1000000;
+        int n = Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine());
+        String multiply = (String.valueOf(n)); // 인덱스 추출을 위해 casting
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        while (st.hasMoreTokens()) {
-            int s = Integer.parseInt(st.nextToken());
+        int[] count = new int[10];
 
-            if (s > maxNum) {
-                maxNum = s;
-            }
-            if (s < minNum) {
-                minNum = s;
+        int count0 = 0, count1 = 0, count2 = 0, count3 = 0, count4 = 0, count5 = 0, count6 = 0, count7 = 0, count8 = 0, count9 = 0;
+
+        char[] arr = multiply.toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < count.length; j++) {
+                if ((arr[i] - '0') == j) { // char형을 int로 바꾸기 위해 "- '0'" 사용
+                    count[j]++;
+                }
             }
         }
+        for (int i = 0; i < count.length; i++) {
+            bw.write(count[i] + "\n");
+        }
 
-        bw.write(String.valueOf(minNum + " " + maxNum));
 
         bw.flush();
         bw.close();
