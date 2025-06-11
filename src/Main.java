5655253542b8ce7510411d96a283ad1133466833
BangJeongBin <bangjungbin@gmail.com>
2025-06-11 17:06:33 +0900

    Baekjoon_1978

--- src/Main.java
+++ src/Main.java
@@ -7,38 +7,34 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine()); // 참가자의 수
+        br.readLine();	// N 은 쓰지 않음.
+        int count = 0;
 
-        int[] sizes = new int[6];
-        StringTokenizer st1 = new StringTokenizer(br.readLine(), " "); // 사이즈별 신청자 수
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
 
-        for (int i = 0; i < sizes.length; i++) {
-            sizes[i] = Integer.parseInt(st1.nextToken());
+        while(st.hasMoreTokens()) {
+
+            // 소수인경우 true, 아닌경우 false
+            boolean isPrime = true;
+
+            int num = Integer.parseInt(st.nextToken());
+
+            if(num == 1) {
+                continue;
+            }
+            for(int i = 2; i <= Math.sqrt(num); i++) { // 제곱근을 이용한 방법
+                if(num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime) {
+                count++;
+            }
         }
-
-        StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
-        int t = Integer.parseInt(st2.nextToken()); // 티셔츠 묶음 수
-        int p = Integer.parseInt(st2.nextToken()); // 펜 묶음 수
-
-        int answer1 = 0;
-        for (int i = 0; i < sizes.length; i++) {
-            answer1 += (int)Math.ceil((double)sizes[i] / t); // Math 클래스의 ceil 메서드를 사용하여 올림을 함
-            // 반드시 ceil 메서드를 사용하는 경우 아큐먼트로 double를 보내주어야 한다.
-            // 참조 : https://luanaeun.tistory.com/208
-
-//            bw.write(i + "번째 계산 : " + answer1 + "\n");
-        }
-
-        int answer2 = n / p;
-        int answer3 = n % p;
-
-        bw.write(answer1 + "\n");
-        bw.write(answer2 + " " + answer3);
-
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(count);
     }
+    // 참조 : https://st-lab.tistory.com/80
 
 
     public static void main(String[] args) throws IOException {
