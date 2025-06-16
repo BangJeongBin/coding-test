c0b92f474dbc74f0a09a21de7e58427e3b8422e9
BangJeongBin <bangjungbin@gmail.com>
2025-06-16 20:16:52 +0900

    LeetCode_14

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -9,65 +9,19 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         // Testcase
-        String s = "MCMXCIV";
+        String[] strs = {"flower", "flow", "flight"};
 
         //--------------------------------------------------------------------------------------------------------------
 
-        int result = 0;
-        char[] str = s.toCharArray();
+        // 해당하는 단어를
+        String result = strs[0];
 
-        for (char c : str) {
-            switch (c) {
-                case 'I':
-                    result += 1;
-                    break;
-                case 'V':
-                    result += 5;
-                    break;
-                case 'X':
-                    result += 10;
-                    break;
-                case 'L':
-                    result += 50;
-                    break;
-                case 'C':
-                    result += 100;
-                    break;
-                case 'D':
-                    result += 500;
-                    break;
-                case 'M':
-                    result += 1000;
-                    break;
+        for(int i = 1; i < strs.length; i++){
+            while(strs[i].indexOf(result) != 0){ // 일치하는 index에 일치하는 문자가 있는 경우
+                result = result.substring(0, result.length() - 1);
+                //bw.write(result + "\n");
             }
         }
-        //bw.write("for문 나온 후 : " + result + "\n");
-
-        if (s.contains("IV")) {
-            result -= 6;
-            result += 4;
-        }
-        if (s.contains("IX")) {
-            result -= 11;
-            result += 9;
-        }
-        if (s.contains("XL")) {
-            result -= 60;
-            result += 40;
-        }
-        if (s.contains("XC")) {
-            result -= 110;
-            result += 90;
-        }
-        if (s.contains("CD")) {
-            result -= 600;
-            result += 400;
-        }
-        if (s.contains("CM")) {
-            result -= 1100;
-            result += 900;
-        }
-        //bw.write("if문 나온 후 : " + result);
         //return result;
 
         //--------------------------------------------------------------------------------------------------------------
