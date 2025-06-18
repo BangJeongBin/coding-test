ee861a56d213b018dff1fa69f5ae18acc59f4c45
BangJeongBin <bangjungbin@gmail.com>
2025-06-18 16:40:24 +0900

    Baekjoon_2798

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -1,28 +1,63 @@
 package baekjoon;
 
 import java.io.*;
+import java.util.*;
 
-// 참조 : https://st-lab.tistory.com/73
+// 참조 : https://st-lab.tistory.com/97
 public class Main {
 
     public static void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int N = Integer.parseInt(br.readLine());
-        int count = 1; // 겹 수(최소 루트)
-        int range = 2;	// 범위 (최솟값 기준)
+        int N = Integer.parseInt(st.nextToken()); // 카드의 개수
+        int M = Integer.parseInt(st.nextToken()); // 목표 값
 
-        if (N == 1) {
-            System.out.print(1);
+        int[] arr = new int[N]; // 입력받은 정수
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        else {
-            while (range <= N) {    // 범위가 N보다 커지기 직전까지 반복
-                range = range + (6 * count);
-                count++;
+        int result = search(arr, N, M);
+        System.out.println(result);
+
+        br.close();
+    }
+
+
+    // 탐색
+    static int search(int[] arr, int N, int M) {
+        int result = 0;
+
+        // 3개를 고르기 때문에 첫번째 카드는 N - 2 까지만 순회
+        for (int i = 0; i < N - 2; i++) {
+
+            // 두 번째 카드는 첫 번째 카드 다음부터 N - 1 까지만 순회
+            for (int j = i + 1; j < N - 1; j++) {
+
+                // 세 번째 카드는 두 번째 카드 다음부터 N 까지 순회
+                for (int k = j + 1; k < N; k++) {
+
+                    // 3개 카드의 합 변수 temp
+                    int temp = arr[i] + arr[j] + arr[k];
+
+                    // M과 세 카드의 합이 같다면 temp return 및 종료
+                    if (M == temp) {
+                        return temp;
+                    }
+
+                    // 세 카드의 합이 이전 합보다 크면서 M 보다 작을 경우 result 갱신
+                    if(result < temp && temp < M) {
+                        result = temp;
+                    }
+                }
             }
-            System.out.print(count);
         }
+
+        // 모든 순회를 마치면 result 리턴
+        return result;
     }
 
 
--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -13,26 +13,26 @@ public class Main {
 
         //--------------------------------------------------------------------------------------------------------------
 
-        if(list1 == null) //return list2;
-        if(list2 == null) //return list1;
-
-        ListNode result = new ListNode();
-        ListNode temp = result;
-
-        while(list1 != null && list2 != null) {
-            if(list1.val > list2.val) {
-                temp.next = list2;
-                list2 = list2.next;
-            }
-            else {
-                temp.next = list1;
-                list1 = list1.next;
-            }
-            temp = temp.next;
-        }
-
-        if(list1 == null) {temp.next = list2;}
-        else {temp.next = list1;}
+//        if(list1 == null) //return list2;
+//        if(list2 == null) //return list1;
+//
+//        ListNode result = new ListNode();
+//        ListNode temp = result;
+//
+//        while(list1 != null && list2 != null) {
+//            if(list1.val > list2.val) {
+//                temp.next = list2;
+//                list2 = list2.next;
+//            }
+//            else {
+//                temp.next = list1;
+//                list1 = list1.next;
+//            }
+//            temp = temp.next;
+//        }
+//
+//        if(list1 == null) {temp.next = list2;}
+//        else {temp.next = list1;}
 
         //return result.next;
 
