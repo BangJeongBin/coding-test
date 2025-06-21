1d4f1203b0a556d1ad720ae97455fc75dc099218
BangJeongBin <bangjungbin@gmail.com>
2025-06-21 21:47:21 +0900

    LeetCode_28

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,7 +3,6 @@ package leetcode;
 import java.io.*;
 import java.util.*;
 
-// 참조 : https://jini-space.tistory.com/72
 public class Main {
 
     public static void solution() throws IOException {
@@ -11,32 +10,17 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         // Testcase
-        int[] nums = {0,1,2,2,3,0,4,2};
-        int val = 2;
+        String haystack = "hello";
+        String needle = "ll";
 
         //--------------------------------------------------------------------------------------------------------------
 
-        List<Integer> list = new ArrayList<>();
-
-        for (int num : nums) {
-            list.add(num);
-        }
-
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i) == val) {
-                list.remove(i);
-                i--; // list.remove()는 삭제되면 index가 하나 밀려나 건너 뛰어지게 된다. 이 방법 아니면 뒤에서 부터 시작하는 방법도 있다.
-            }
-        }
-
-        for (int i = 0; i < list.size(); i++) {
-            nums[i] = list.get(i);
-        }
-
-        //return list.size();
-        bw.write(String.valueOf(list.size()) + "\n");
-        for (int num : list) {
-            bw.write("num : " + num + "\n");
+        if (haystack.contains(needle)) {
+            bw.write(String.valueOf(haystack.indexOf("sad")));
+            //return haystack.indexOf(needle);
+        } else {
+            bw.write("false");
+            //return -1;
         }
 
         //--------------------------------------------------------------------------------------------------------------
