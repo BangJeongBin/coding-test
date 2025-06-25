c89437d693c09f588304ba931d42732086e6a25e
BangJeongBin <bangjungbin@gmail.com>
2025-06-25 16:40:11 +0900

    LeetCode_67

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,7 +3,6 @@ package leetcode;
 import java.io.*;
 import java.util.*;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0000-0099/0066.Plus%20One/Solution.java
 public class Main {
 
     public static void solution() throws IOException {
@@ -11,58 +10,67 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         // Testcase
-        int[] digits = {7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,6};
+        String a = "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101";
+        String b = "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011";
 
         //--------------------------------------------------------------------------------------------------------------
         // Solution 1
 
-        int n = digits.length; // testcase 길이
+        StringBuilder sb = new StringBuilder();
 
-        for (int i = n - 1; i >= 0; --i) {
-            ++digits[i];
-            digits[i] %= 10;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
 
-            if (digits[i] != 0) { // 마지막 인덱스가 9가 아닌 경우
-                //return digits;
-            }
+        while (i >= 0 || j >= 0 || carry == 1) {
+            int sum = carry;
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
+
+            sb.append(sum % 2);
+            carry = sum / 2;
         }
 
-        // 마지막 인덱스가 9인 경우
-        digits = new int[n + 1];
-        digits[0] = 1;
-        //return digits;
+        System.out.println(sb.reverse().toString());
+        //return sb.reverse().toString();
 
         //--------------------------------------------------------------------------------------------------------------
         // Solution 2
-        // ● Long 보다 큰 수가 나와서 pass 못함
+        // Long을 초과하는 자릿수는 해결 안됨
 
-        int digitLen = digits.length; // testcase 길이
-        int strLen = 0; // +1 연산 후 길이
-        String str = "";
-
-        for (int digit : digits) {
-            str += digit;
-        }
-        // 배열을 풀어서 연산 후 다시 문자열로 캐스팅
-        String result = (Long.parseLong(str) + 1) + "";
-        strLen = result.length();
-
-        if (digitLen != strLen) {
-            // 배열을 복사하여 다시 할당.
-            digits = Arrays.copyOf(digits, digitLen + 1);
-        }
-
-        for (int i = 0; i < digits.length; i++) {
-            digits[i] = result.charAt(i) - '0';
-        }
-
-        for (int digit : digits) {
-            System.out.println("digit : " + digit);
-        }
-        //return digits;
+//        if (a.equals("0") && b.equals("0")) {
+//            //return "0";
+//        }
+//
+//        char[] ac = a.toCharArray();
+//        char[] bc = b.toCharArray();
+//
+//        int sum1 = 1, sum2 = 1;
+//        Long ai = 0L, bi = 0L;
+//
+//        // testcase a, b 를 10진수로 바꾸는 코드
+//        for (int i = ac.length - 1; i >= 0; i--) {
+//            ai += (ac[i] - '0') * sum1;
+//            sum1 *= 2;
+//        }
+//        for (int i = bc.length - 1; i >= 0; i--) {
+//            bi += (bc[i] - '0') * sum2;
+//            sum2 *= 2;
+//        }
+//
+//        Long result = ai + bi;
+//        String binary = "";
+//
+//        // a + b 의 값을 다시 2 진수로 바꾸는 코드
+//        while (result > 0) {
+//            bw.write("result : " + result);
+//            binary = (result % 2) + binary;
+//            result = result / 2;
+//            bw.write(", binary : " + binary + "\n");
+//        }
+//        //return binary;
 
         //--------------------------------------------------------------------------------------------------------------
-
 
         bw.flush();
         bw.close();
