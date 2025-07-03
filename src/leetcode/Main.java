6bb072a039766ca7a07c4017d3d5abdf9a5ff927
BangJeongBin <bangjungbin@gmail.com>
2025-07-03 20:58:19 +0900

    LeetCode_3304

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,7 +3,7 @@ package leetcode;
 import java.io.*;
 import java.util.*;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0000-0099/0069.Sqrt(x)/Solution.java
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/3300-3399/3304.Find%20the%20K-th%20Character%20in%20String%20Game%20I/Solution.java
 public class Main {
 
     public static void solution() throws IOException {
@@ -11,21 +11,19 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         // Testcase
-        int x = 2;
+        int k = 5;
 
         //--------------------------------------------------------------------------------------------------------------
 
-        int l = 0, r = x;
-        while (l < r) {
-            int mid = (l + r + 1) >>> 1;
-            if (mid > x / mid) {
-                r = mid - 1;
-            } else {
-                l = mid;
+        List<Integer> word = new ArrayList<>();
+        word.add(0);
+        while (word.size() < k) {
+            for (int i = 0, m = word.size(); i < m; ++i) {
+                word.add((word.get(i) + 1) % 26);
             }
         }
-        System.out.println(x);
-        //return l;
+        System.out.println((char) ('a' + word.get(k - 1)));
+        //return (char) ('a' + word.get(k - 1));
 
         //--------------------------------------------------------------------------------------------------------------
 
