2be2b209f215bb67e50e165a1695013d324928d4
BangJeongBin <bangjungbin@gmail.com>
2025-07-08 20:34:42 +0900

    LeetCode_1751

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,41 +3,58 @@ package leetcode;
 import java.io.*;
 import java.util.*;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/3300-3399/3307.Find%20the%20K-th%20Character%20in%20String%20Game%20II/Solution.java
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/1700-1799/1751.Maximum%20Number%20of%20Events%20That%20Can%20Be%20Attended%20II/Solution.java
 public class Main {
+
+    private int[][] events;
+    private int[][] f;
+    private int n;
 
     public static void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         // Testcase
-        int k = 5;
-        int[] operations = {0, 0, 0};
+
 
         //--------------------------------------------------------------------------------------------------------------
 
-        long n = 1;
-        int i = 0;
-        while (n < k) {
-            n *= 2;
-            ++i;
-        }
-        int d = 0;
-        while (n > 1) {
-            if (k > n / 2) {
-                k -= n / 2;
-                d += operations[i - 1];
-            }
-            n /= 2;
-            --i;
-        }
-        //return (char) ('a' + (d % 26));
+        Arrays.sort(events, (a, b) -> a[0] - b[0]);
+        this.events = events;
+        n = events.length;
+        f = new int[n][k + 1];
+        //return dfs(0, k);
 
         //--------------------------------------------------------------------------------------------------------------
 
         bw.flush();
         bw.close();
         br.close();
+    }
+
+    private int dfs(int i, int k) {
+        if (i >= n || k <= 0) {
+            return 0;
+        }
+        if (f[i][k] != 0) {
+            return f[i][k];
+        }
+        int j = search(events, events[i][1], i + 1);
+        int ans = Math.max(dfs(i + 1, k), dfs(j, k - 1) + events[i][2]);
+        return f[i][k] = ans;
+    }
+
+    private int search(int[][] events, int x, int lo) {
+        int l = lo, r = n;
+        while (l < r) {
+            int mid = (l + r) >> 1;
+            if (events[mid][0] > x) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
     }
 
 
