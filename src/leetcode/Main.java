105a5a127d80d4e91d70176d27a2561b46282e33
BangJeongBin <bangjungbin@gmail.com>
2025-07-13 15:41:59 +0900

    LeetCode_2410

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,7 +3,7 @@ package leetcode;
 import java.io.*;
 import java.util.*;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/1700-1799/1751.Maximum%20Number%20of%20Events%20That%20Can%20Be%20Attended%20II/Solution.java
+// 참조 : https://leetcode.com/problems/maximum-matching-of-players-with-trainers/description/?envType=daily-question&envId=2025-07-13
 public class Main {
 
     private int[][] events;
@@ -15,46 +15,29 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         // Testcase
-
+        int[] players = {4,7,9};
+        int[] trainers = {8,2,5,9};
 
         //--------------------------------------------------------------------------------------------------------------
 
-        Arrays.sort(events, (a, b) -> a[0] - b[0]);
-        this.events = events;
-        n = events.length;
-        f = new int[n][k + 1];
-        //return dfs(0, k);
+        Arrays.sort(players);
+        Arrays.sort(trainers);
+        int m = players.length, n = trainers.length;
+        for (int i = 0, j = 0; i < m; ++i, ++j) {
+            while (j < n && trainers[j] < players[i]) {
+                ++j;
+            }
+            if (j == n) {
+                //return i;
+            }
+        }
+        //return m;
 
         //--------------------------------------------------------------------------------------------------------------
 
         bw.flush();
         bw.close();
         br.close();
-    }
-
-    private int dfs(int i, int k) {
-        if (i >= n || k <= 0) {
-            return 0;
-        }
-        if (f[i][k] != 0) {
-            return f[i][k];
-        }
-        int j = search(events, events[i][1], i + 1);
-        int ans = Math.max(dfs(i + 1, k), dfs(j, k - 1) + events[i][2]);
-        return f[i][k] = ans;
-    }
-
-    private int search(int[][] events, int x, int lo) {
-        int l = lo, r = n;
-        while (l < r) {
-            int mid = (l + r) >> 1;
-            if (events[mid][0] > x) {
-                r = mid;
-            } else {
-                l = mid + 1;
-            }
-        }
-        return l;
     }
 
 
