affc30a899be356762193cebdd948899e14ca0a7
BangJeongBin <bangjungbin@gmail.com>
2025-07-16 20:34:04 +0900

    LeetCode_3201

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,7 +3,7 @@ package leetcode;
 import java.io.*;
 import java.util.*;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/3100-3199/3136.Valid%20Word/Solution.java
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/3200-3299/3201.Find%20the%20Maximum%20Length%20of%20Valid%20Subsequence%20I/Solution.java
 public class Main {
 
     private int[][] events;
@@ -15,30 +15,22 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         // Testcase
-        String word = "234Adas";
+        int[] nums = {1, 2, 3, 4};
 
         //--------------------------------------------------------------------------------------------------------------
 
-        if (word.length() < 3) {
-            //return false;
-        }
-        boolean hasVowel = false, hasConsonant = false;
-        boolean[] vs = new boolean[26];
-        for (char c : "aeiou".toCharArray()) {
-            vs[c - 'a'] = true;
-        }
-        for (char c : word.toCharArray()) {
-            if (Character.isAlphabetic(c)) {
-                if (vs[Character.toLowerCase(c) - 'a']) {
-                    hasVowel = true;
-                } else {
-                    hasConsonant = true;
-                }
-            } else if (!Character.isDigit(c)) {
-                //return false;
+        int k = 2;
+        int[][] f = new int[k][k];
+        int ans = 0;
+        for (int x : nums) {
+            x %= k;
+            for (int j = 0; j < k; ++j) {
+                int y = (j - x + k) % k;
+                f[x][y] = f[y][x] + 1;
+                ans = Math.max(ans, f[x][y]);
             }
         }
-        //return hasVowel && hasConsonant;
+        //return ans;
 
         //--------------------------------------------------------------------------------------------------------------
 
