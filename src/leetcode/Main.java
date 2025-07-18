367c58979f108ba4db38b52273afbe92a9378fec
BangJeongBin <bangjungbin@gmail.com>
2025-07-18 14:09:22 +0900

    LeetCode_2163

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,7 +3,7 @@ package leetcode;
 import java.io.*;
 import java.util.*;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/3200-3299/3202.Find%20the%20Maximum%20Length%20of%20Valid%20Subsequence%20II/Solution.java
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/2100-2199/2163.Minimum%20Difference%20in%20Sums%20After%20Removal%20of%20Elements/Solution.java
 public class Main {
 
     private int[][] events;
@@ -15,20 +15,40 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         // Testcase
-        int[] nums = {1,2,3,4,5};
+        int[] nums = {3, 1, 2};
         int k = 2;
 
         //--------------------------------------------------------------------------------------------------------------
 
-        int[][] f = new int[k][k];
-        int ans = 0;
-        for (int x : nums) {
-            x %= k;
-            for (int j = 0; j < k; ++j) {
-                int y = (j - x + k) % k;
-                f[x][y] = f[y][x] + 1;
-                ans = Math.max(ans, f[x][y]);
+        int m = nums.length;
+        int n = m / 3;
+        long s = 0;
+        long[] pre = new long[m + 1];
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        for (int i = 1; i <= n * 2; ++i) {
+            int x = nums[i - 1];
+            s += x;
+            pq.offer(x);
+            if (pq.size() > n) {
+                s -= pq.poll();
             }
+            pre[i] = s;
+        }
+        s = 0;
+        long[] suf = new long[m + 1];
+        pq = new PriorityQueue<>();
+        for (int i = m; i > n; --i) {
+            int x = nums[i - 1];
+            s += x;
+            pq.offer(x);
+            if (pq.size() > n) {
+                s -= pq.poll();
+            }
+            suf[i] = s;
+        }
+        long ans = 1L << 60;
+        for (int i = n; i <= n * 2; ++i) {
+            ans = Math.min(ans, pre[i] - suf[i + 1]);
         }
         //return ans;
 
