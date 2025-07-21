55d1d9ad428cfc00b019c59d6678532597eba401
BangJeongBin <bangjungbin@gmail.com>
2025-07-21 20:17:00 +0900

    LeetCode_70

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,54 +3,25 @@ package leetcode;
 import java.io.*;
 import java.util.*;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/2100-2199/2163.Minimum%20Difference%20in%20Sums%20After%20Removal%20of%20Elements/Solution.java
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0000-0099/0070.Climbing%20Stairs/Solution.java
 public class Main {
-
-    private int[][] events;
-    private int[][] f;
-    private int n;
 
     public static void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         // Testcase
-        int[] nums = {3, 1, 2};
-        int k = 2;
+        int n = 3;
 
         //--------------------------------------------------------------------------------------------------------------
 
-        int m = nums.length;
-        int n = m / 3;
-        long s = 0;
-        long[] pre = new long[m + 1];
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
-        for (int i = 1; i <= n * 2; ++i) {
-            int x = nums[i - 1];
-            s += x;
-            pq.offer(x);
-            if (pq.size() > n) {
-                s -= pq.poll();
-            }
-            pre[i] = s;
+        int a = 0, b = 1;
+        for (int i = 0; i < n; ++i) {
+            int c = a + b;
+            a = b;
+            b = c;
         }
-        s = 0;
-        long[] suf = new long[m + 1];
-        pq = new PriorityQueue<>();
-        for (int i = m; i > n; --i) {
-            int x = nums[i - 1];
-            s += x;
-            pq.offer(x);
-            if (pq.size() > n) {
-                s -= pq.poll();
-            }
-            suf[i] = s;
-        }
-        long ans = 1L << 60;
-        for (int i = n; i <= n * 2; ++i) {
-            ans = Math.min(ans, pre[i] - suf[i + 1]);
-        }
-        //return ans;
+        //return b;
 
         //--------------------------------------------------------------------------------------------------------------
 
