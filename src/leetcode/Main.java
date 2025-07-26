e182d1e4b5907432dbaa9ac1d159226e865a7668
BangJeongBin <bangjungbin@gmail.com>
2025-07-26 21:19:56 +0900

    LeetCode_111

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,7 +3,7 @@ package leetcode;
 import java.io.*;
 import java.util.*;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0100-0199/0110.Balanced%20Binary%20Tree/Solution.java
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0100-0199/0111.Minimum%20Depth%20of%20Binary%20Tree/Solution.java
 public class Main {
 
     public static void solution() throws IOException {
@@ -27,11 +27,20 @@ public class Main {
          */
 
         // Testcase
-        //TreeNode root = {3, 9, 20, null, null, 15, 7};
+        //TreeNode root = {3,9,20,null,null,15,7};
 
         //--------------------------------------------------------------------------------------------------------------
 
-        //return height(root) >= 0;
+//        if (root == null) {
+//            return 0;
+//        }
+//        if (root.left == null) {
+//            return 1 + minDepth(root.right);
+//        }
+//        if (root.right == null) {
+//            return 1 + minDepth(root.left);
+//        }
+//        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
 
         //--------------------------------------------------------------------------------------------------------------
 
@@ -39,18 +48,6 @@ public class Main {
         bw.close();
         br.close();
     }
-
-//    private int height(TreeNode root) {
-//        if (root == null) {
-//            return 0;
-//        }
-//        int l = height(root.left);
-//        int r = height(root.right);
-//        if (l == -1 || r == -1 || Math.abs(l - r) > 1) {
-//            return -1;
-//        }
-//        return 1 + Math.max(l, r);
-//    }
 
 
     public static void main(String[] args) throws IOException {
