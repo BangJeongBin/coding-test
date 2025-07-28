bcfda22b42a0e7840a42e2d53aac0f6eee157ed3
BangJeongBin <bangjungbin@gmail.com>
2025-07-28 13:23:16 +0900

    LeetCode_118

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,36 +3,43 @@ package leetcode;
 import java.io.*;
 import java.util.*;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0100-0199/0111.Minimum%20Depth%20of%20Binary%20Tree/Solution.java
+// 참조 :
 public class Main {
 
     public static void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        /**
-         * Definition for a binary tree node.
-         * public class TreeNode {
-         *     int val;
-         *     TreeNode left;
-         *     TreeNode right;
-         *     TreeNode() {}
-         *     TreeNode(int val) { this.val = val; }
-         *     TreeNode(int val, TreeNode left, TreeNode right) {
-         *         this.val = val;
-         *         this.left = left;
-         *         this.right = right;
-         *     }
-         * }
-         */
-
         // Testcase
-        //TreeNode root = {5,4,8,11,null,13,4,7,2,null,null,null,1};
-        int targetSum = 22;
+        int numRows = 5;
 
         //--------------------------------------------------------------------------------------------------------------
 
-        //return dfs(root, targetSum);
+        int[][] triangle = new int[numRows][numRows]; // 실제 연산용 변수
+        List<List<Integer>> result = new ArrayList<>(); // 리턴용 변수
+
+        for (int i = 0; i < numRows; i++) {
+            triangle[i][0] = 1; // 초기값 지정
+            triangle[i][i] = 1; // ,,
+            for (int j = 1; j < i; j++) {
+                triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+            }
+        }
+
+        for (int i = 0; i < numRows; i++) { // 2차원 배열을 2차원 리스트로 복사
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j < triangle.length; j++) {
+
+                if (triangle[i][j] != 0) {
+                    row.add(triangle[i][j]);
+                } else {
+                    break;
+                }
+            }
+            result.add(row);
+        }
+
+        //return result;
 
         //--------------------------------------------------------------------------------------------------------------
 
@@ -40,17 +47,6 @@ public class Main {
         bw.close();
         br.close();
     }
-
-//    private boolean dfs(TreeNode root, int s) {
-//        if (root == null) {
-//            return false;
-//        }
-//        s -= root.val;
-//        if (root.left == null && root.right == null && s == 0) {
-//            return true;
-//        }
-//        return dfs(root.left, s) || dfs(root.right, s);
-//    }
 
 
     public static void main(String[] args) throws IOException {
