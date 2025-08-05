918478b7cc4db4df215abd07636942bc28d500ce
BangJeongBin <bangjungbin@gmail.com>
2025-08-05 13:27:21 +0900

    LeetCode_144

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -1,7 +1,6 @@
 package leetcode;
 
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
 
 // 참조 : https://github.com/doocs/leetcode/blob/main/solution/0100-0199/0141.Linked%20List%20Cycle/Solution2.java
 public class Main {
@@ -9,32 +8,41 @@ public class Main {
     public static void solution() throws IOException {
 
         /**
-         * Definition for singly-linked list.
-         * class ListNode {
+         * Definition for a binary tree node.
+         * public class TreeNode {
          *     int val;
-         *     ListNode next;
-         *     ListNode(int x) {
-         *         val = x;
-         *         next = null;
+         *     TreeNode left;
+         *     TreeNode right;
+         *     TreeNode() {}
+         *     TreeNode(int val) { this.val = val; }
+         *     TreeNode(int val, TreeNode left, TreeNode right) {
+         *         this.val = val;
+         *         this.left = left;
+         *         this.right = right;
          *     }
          * }
          */
 
         // Testcase
-        //ListNode head = {3,2,0,-4};
+        //ListNode root = {1, null, 2, 3};
 
         //--------------------------------------------------------------------------------------------------------------
 
-//        ListNode slow = head;
-//        ListNode fast = head;
-//        while (fast != null && fast.next != null) {
-//            slow = slow.next;
-//            fast = fast.next.next;
-//            if (slow == fast) {
-//                return true;
-//            }
+//        private List<Integer> ans = new ArrayList<>();
+//
+//        public List<Integer> preorderTraversal(TreeNode root) {
+//            dfs(root);
+//            return ans;
 //        }
-//        return false;
+//
+//        private void dfs(TreeNode root) {
+//            if (root == null) {
+//                return;
+//            }
+//            ans.add(root.val);
+//            dfs(root.left);
+//            dfs(root.right);
+//        }
 
         //--------------------------------------------------------------------------------------------------------------
 
