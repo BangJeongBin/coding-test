fc86f88a171c032d9bd8a5034b99b37abfdab6ac
BangJeongBin <bangjungbin@gmail.com>
2025-08-07 16:50:07 +0900

    LeetCode_168

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -2,38 +2,44 @@ package leetcode;
 
 import java.io.IOException;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0100-0199/0145.Binary%20Tree%20Postorder%20Traversal/Solution.java
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0100-0199/0168.Excel%20Sheet%20Column%20Title/Solution.java
 public class Main {
 
     public static void solution() throws IOException {
 
-        /**
-         * Definition for singly-linked list.
-         * public class ListNode {
-         *     int val;
-         *     ListNode next;
-         *     ListNode(int x) {
-         *         val = x;
-         *         next = null;
-         *     }
-         * }
-         */
-
         // Testcase
-        //ListNode headA = {4,1,8,4,5};
-        //ListNode headB = {5,6,1,8,4,5};
+        int columnNumber = 702;
+
+        //--------------------------------------------------------------------------------------------------------------
+        // 1의 자리가 "Z"인 경우 오류 발생. 해결 못함
+        // --columnNumber 을 사용해야 함.
+
+        String result = ""; // 리턴 변수
+
+        if (columnNumber < 27) { // 답이 한 자리인 경우
+            char ch = (char) (columnNumber + 64);
+            //return String.valueOf(ch);
+        } else { // 답이 한 자리 이상 일경우
+            while (columnNumber > 26) {
+                result = (char) (((columnNumber % 26) + 64) == 64 ? 90 : ((columnNumber % 26) + 64)) + result;
+                columnNumber /= 27;
+            }
+            result = (char) (columnNumber + 64) + result;
+            //return result;
+            System.out.println("result = " + result);
+        }
 
         //--------------------------------------------------------------------------------------------------------------
 
-//        ListNode a = headA, b = headB;
-//        while (a != b) {
-//            a = a == null ? headB : a.next;
-//            b = b == null ? headA : b.next;
-//        }
-//        return a;
+        StringBuilder res = new StringBuilder();
+        while (columnNumber != 0) {
+            --columnNumber;
+            res.append((char) ('A' + columnNumber % 26));
+            columnNumber /= 26;
+        }
+        //return res.reverse().toString();
 
         //--------------------------------------------------------------------------------------------------------------
-
     }
 
 
