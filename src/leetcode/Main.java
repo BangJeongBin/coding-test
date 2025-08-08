eb9d592397700c7d8195973d51d7f58393d6bb37
BangJeongBin <bangjungbin@gmail.com>
2025-08-08 11:47:13 +0900

    LeetCode_169

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -1,43 +1,38 @@
 package leetcode;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0100-0199/0168.Excel%20Sheet%20Column%20Title/Solution.java
+// 참조 :
 public class Main {
 
     public static void solution() throws IOException {
 
         // Testcase
-        int columnNumber = 702;
+        int[] nums = {2, 2, 1, 1, 1, 2, 2};
 
         //--------------------------------------------------------------------------------------------------------------
-        // 1의 자리가 "Z"인 경우 오류 발생. 해결 못함
-        // --columnNumber 을 사용해야 함.
 
-        String result = ""; // 리턴 변수
+        List<Integer> list = new ArrayList<>();
+        int index = 0;
 
-        if (columnNumber < 27) { // 답이 한 자리인 경우
-            char ch = (char) (columnNumber + 64);
-            //return String.valueOf(ch);
-        } else { // 답이 한 자리 이상 일경우
-            while (columnNumber > 26) {
-                result = (char) (((columnNumber % 26) + 64) == 64 ? 90 : ((columnNumber % 26) + 64)) + result;
-                columnNumber /= 27;
+        for (int num : nums) { // 리스트 초기화
+            list.add(num);
+        }
+        Collections.sort(list); // 리스트 정렬
+
+        while (index < list.size()) {
+            int qty = Collections.frequency(list, list.get(index)); // 해당 값의 개수를 추출
+            if (qty > (list.size() / 2)) { // 과반수일 경우 리턴
+                //return list.get(index);
+                System.out.println(list.get(index));
             }
-            result = (char) (columnNumber + 64) + result;
-            //return result;
-            System.out.println("result = " + result);
+            // 아닐 경우 해당값의 마지막 인덱스를 추출하고 그 다음 값으로 중복 뛰어넘기
+            index += list.lastIndexOf(list.get(index)) + 1;
         }
-
-        //--------------------------------------------------------------------------------------------------------------
-
-        StringBuilder res = new StringBuilder();
-        while (columnNumber != 0) {
-            --columnNumber;
-            res.append((char) ('A' + columnNumber % 26));
-            columnNumber /= 26;
-        }
-        //return res.reverse().toString();
+        //return 0;
 
         //--------------------------------------------------------------------------------------------------------------
     }
