feeab5c6ffe1ea823e2fc3c3eaf4aef2f5f74376
BangJeongBin <bangjungbin@gmail.com>
2025-08-15 20:54:30 +0900

    LeetCode_175

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -2,23 +2,36 @@ package leetcode;
 
 import java.io.IOException;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/2200-2299/2264.Largest%203-Same-Digit%20Number%20in%20String/Solution.java
+// 참조 :
 public class Main {
 
     public static void solution() throws IOException {
 
         // Testcase
-        String num = "6777133339";
+
+        // "Person" table:
+        //+----------+----------+-----------+
+        //| personId | lastName | firstName |
+        //+----------+----------+-----------+
+        //| 1        | Wang     | Allen     |
+        //| 2        | Alice    | Bob       |
+        //+----------+----------+-----------+
+
+        // "Address" table:
+        //+-----------+----------+---------------+------------+
+        //| addressId | personId | city          | state      |
+        //+-----------+----------+---------------+------------+
+        //| 1         | 2        | New York City | New York   |
+        //| 2         | 3        | Leetcode      | California |
+        //+-----------+----------+---------------+------------+
+
 
         //--------------------------------------------------------------------------------------------------------------
 
-        for (int i = 9; i >= 0; i--) {
-            String s = String.valueOf(i).repeat(3);
-            if (num.contains(s)) {
-                //return s;
-            }
-        }
-        //return "";
+        // SELECT p.firstName, p.lastName, a.city, a.state
+        // FROM Person p
+        // LEFT OUTER JOIN Address a
+        // ON p.personId = a.personId;
 
         //--------------------------------------------------------------------------------------------------------------
     }
