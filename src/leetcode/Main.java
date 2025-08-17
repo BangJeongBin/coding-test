faab7ebdf65f82e61b07522c446db3a11f1a950a
BangJeongBin <bangjungbin@gmail.com>
2025-08-17 17:47:49 +0900

    LeetCode_181

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -2,36 +2,30 @@ package leetcode;
 
 import java.io.IOException;
 
-// 참조 :
+// 참조 : https://github.com/doocs/leetcode/tree/main/solution/0100-0199/0181.Employees%20Earning%20More%20Than%20Their%20Managers
 public class Main {
 
     public static void solution() throws IOException {
 
         // Testcase
 
-        // "Person" table:
-        //+----------+----------+-----------+
-        //| personId | lastName | firstName |
-        //+----------+----------+-----------+
-        //| 1        | Wang     | Allen     |
-        //| 2        | Alice    | Bob       |
-        //+----------+----------+-----------+
-
-        // "Address" table:
-        //+-----------+----------+---------------+------------+
-        //| addressId | personId | city          | state      |
-        //+-----------+----------+---------------+------------+
-        //| 1         | 2        | New York City | New York   |
-        //| 2         | 3        | Leetcode      | California |
-        //+-----------+----------+---------------+------------+
-
+        // "Employee" table:
+        //+----+-------+--------+-----------+
+        //| id | name  | salary | managerId |
+        //+----+-------+--------+-----------+
+        //| 1  | Joe   | 70000  | 3         |
+        //| 2  | Henry | 80000  | 4         |
+        //| 3  | Sam   | 60000  | Null      |
+        //| 4  | Max   | 90000  | Null      |
+        //+----+-------+--------+-----------+
 
         //--------------------------------------------------------------------------------------------------------------
 
-        // SELECT p.firstName, p.lastName, a.city, a.state
-        // FROM Person p
-        // LEFT OUTER JOIN Address a
-        // ON p.personId = a.personId;
+        //SELECT e1.name Employee
+        //FROM
+        //    Employee e1
+        //    JOIN Employee e2 ON e1.managerId = e2.id
+        //WHERE e1.salary > e2.salary;
 
         //--------------------------------------------------------------------------------------------------------------
     }
