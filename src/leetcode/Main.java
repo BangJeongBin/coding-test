577a3ed4f35a160ec295d7f4bded08620c8e62b2
BangJeongBin <bangjungbin@gmail.com>
2025-08-18 11:37:21 +0900

    LeetCode_182

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -2,30 +2,28 @@ package leetcode;
 
 import java.io.IOException;
 
-// 참조 : https://github.com/doocs/leetcode/tree/main/solution/0100-0199/0181.Employees%20Earning%20More%20Than%20Their%20Managers
+// 참조 :
 public class Main {
 
     public static void solution() throws IOException {
 
         // Testcase
 
-        // "Employee" table:
-        //+----+-------+--------+-----------+
-        //| id | name  | salary | managerId |
-        //+----+-------+--------+-----------+
-        //| 1  | Joe   | 70000  | 3         |
-        //| 2  | Henry | 80000  | 4         |
-        //| 3  | Sam   | 60000  | Null      |
-        //| 4  | Max   | 90000  | Null      |
-        //+----+-------+--------+-----------+
+        // "Person" table:
+        //+----+---------+
+        //| id | email   |
+        //+----+---------+
+        //| 1  | a@b.com |
+        //| 2  | c@d.com |
+        //| 3  | a@b.com |
+        //+----+---------+
 
         //--------------------------------------------------------------------------------------------------------------
 
-        //SELECT e1.name Employee
-        //FROM
-        //    Employee e1
-        //    JOIN Employee e2 ON e1.managerId = e2.id
-        //WHERE e1.salary > e2.salary;
+        //SELECT email As Email
+        //FROM Person
+        //GROUP BY email
+        //HAVING COUNT(email) >= 2;
 
         //--------------------------------------------------------------------------------------------------------------
     }
