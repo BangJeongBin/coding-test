980977e020071671ca645f5b5c164a8f1f77445b
BangJeongBin <bangjungbin@gmail.com>
2025-08-19 22:57:59 +0900

    LeetCode_2348

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -2,28 +2,26 @@ package leetcode;
 
 import java.io.IOException;
 
-// 참조 :
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/2300-2399/2348.Number%20of%20Zero-Filled%20Subarrays/Solution.java
 public class Main {
 
     public static void solution() throws IOException {
 
         // Testcase
-
-        // "Person" table:
-        //+----+---------+
-        //| id | email   |
-        //+----+---------+
-        //| 1  | a@b.com |
-        //| 2  | c@d.com |
-        //| 3  | a@b.com |
-        //+----+---------+
+        int[] nums = {1, 3, 0, 0, 2, 0, 0, 4};
 
         //--------------------------------------------------------------------------------------------------------------
 
-        //SELECT email As Email
-        //FROM Person
-        //GROUP BY email
-        //HAVING COUNT(email) >= 2;
+        long ans = 0;
+        int cnt = 0;
+        for (int x : nums) {
+            if (x == 0) {
+                ans += ++cnt;
+            } else {
+                cnt = 0;
+            }
+        }
+        //return ans;
 
         //--------------------------------------------------------------------------------------------------------------
     }
