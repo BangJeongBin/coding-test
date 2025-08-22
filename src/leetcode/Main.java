c7d385ff760877a909d99fa3f29fa081e75fbf8c
BangJeongBin <bangjungbin@gmail.com>
2025-08-22 13:20:52 +0900

    LeetCode_3195

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -2,36 +2,30 @@ package leetcode;
 
 import java.io.IOException;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/1500-1599/1504.Count%20Submatrices%20With%20All%20Ones/Solution.java
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/3100-3199/3195.Find%20the%20Minimum%20Area%20to%20Cover%20All%20Ones%20I/Solution.java
 public class Main {
 
     public static void solution() throws IOException {
 
         // Testcase
-        int[][] mat = {{1, 0, 1}, {1, 1, 0}, {1, 1, 0}};
+        int[][] grid = {{0, 1, 0}, {1, 0, 1}};
 
         //--------------------------------------------------------------------------------------------------------------
 
-        int m = mat.length, n = mat[0].length;
-        int[][] g = new int[m][n];
+        int m = grid.length, n = grid[0].length;
+        int x1 = m, y1 = n;
+        int x2 = 0, y2 = 0;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                if (mat[i][j] == 1) {
-                    g[i][j] = j == 0 ? 1 : 1 + g[i][j - 1];
+                if (grid[i][j] == 1) {
+                    x1 = Math.min(x1, i);
+                    y1 = Math.min(y1, j);
+                    x2 = Math.max(x2, i);
+                    y2 = Math.max(y2, j);
                 }
             }
         }
-        int ans = 0;
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                int col = 1 << 30;
-                for (int k = i; k >= 0 && col > 0; --k) {
-                    col = Math.min(col, g[k][j]);
-                    ans += col;
-                }
-            }
-        }
-        //return ans;
+        //return (x2 - x1 + 1) * (y2 - y1 + 1);
 
         //--------------------------------------------------------------------------------------------------------------
     }
