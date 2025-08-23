5c3f0318b3e27cfe5a5a9d1e144c89dbb419c59d
BangJeongBin <bangjungbin@gmail.com>
2025-08-23 11:42:17 +0900

    LeetCode_190

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -2,30 +2,22 @@ package leetcode;
 
 import java.io.IOException;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/3100-3199/3195.Find%20the%20Minimum%20Area%20to%20Cover%20All%20Ones%20I/Solution.java
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0100-0199/0190.Reverse%20Bits/Solution.java
 public class Main {
 
     public static void solution() throws IOException {
 
         // Testcase
-        int[][] grid = {{0, 1, 0}, {1, 0, 1}};
+        int n = 43261596;
 
         //--------------------------------------------------------------------------------------------------------------
 
-        int m = grid.length, n = grid[0].length;
-        int x1 = m, y1 = n;
-        int x2 = 0, y2 = 0;
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (grid[i][j] == 1) {
-                    x1 = Math.min(x1, i);
-                    y1 = Math.min(y1, j);
-                    x2 = Math.max(x2, i);
-                    y2 = Math.max(y2, j);
-                }
-            }
+        int ans = 0;
+        for (int i = 0; i < 32 && n != 0; ++i) {
+            ans |= (n & 1) << (31 - i);
+            n >>>= 1;
         }
-        //return (x2 - x1 + 1) * (y2 - y1 + 1);
+        //return ans;
 
         //--------------------------------------------------------------------------------------------------------------
     }
