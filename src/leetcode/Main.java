5b4bc5ebc8bdd7a239c1f7cff6a2853338bb1b09
BangJeongBin <bangjungbin@gmail.com>
2025-08-25 14:24:27 +0900

    LeetCode_195

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -2,7 +2,7 @@ package leetcode;
 
 import java.io.IOException;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0100-0199/0193.Valid%20Phone%20Numbers/Solution.sh
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0100-0199/0195.Tenth%20Line/Solution.sh
 public class Main {
 
     public static void solution() throws IOException {
@@ -12,7 +12,7 @@ public class Main {
 
         //--------------------------------------------------------------------------------------------------------------
 
-        //awk '/^([0-9]{3}-|\([0-9]{3}\) )[0-9]{3}-[0-9]{4}$/' file.txt
+        //sed -n 10p file.txt
 
         //--------------------------------------------------------------------------------------------------------------
     }
