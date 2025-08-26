335e90bd4b4a7fe2b3794ac51b3c2e8210fa4d0c
BangJeongBin <bangjungbin@gmail.com>
2025-08-26 17:09:41 +0900

    LeetCode_3000

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -2,18 +2,28 @@ package leetcode;
 
 import java.io.IOException;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0100-0199/0196.Delete%20Duplicate%20Emails/Solution3.sql
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/3000-3099/3000.Maximum%20Area%20of%20Longest%20Diagonal%20Rectangle/Solution.java
 public class Main {
 
     public static void solution() throws IOException {
 
         // Testcase
-
+        int[][] dimensions = {{9, 3}, {8, 6}};
 
         //--------------------------------------------------------------------------------------------------------------
 
-//        DELETE FROM Person
-//        WHERE id NOT IN (SELECT MIN(id) FROM (SELECT * FROM Person) AS p GROUP BY email);
+        int ans = 0, mx = 0;
+        for (var d : dimensions) {
+            int l = d[0], w = d[1];
+            int t = l * l + w * w;
+            if (mx < t) {
+                mx = t;
+                ans = l * w;
+            } else if (mx == t) {
+                ans = Math.max(ans, l * w);
+            }
+        }
+        //return ans;
 
         //--------------------------------------------------------------------------------------------------------------
     }
