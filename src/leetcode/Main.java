5663db829347c3f70079ac1aa742bae30eccea02
BangJeongBin <bangjungbin@gmail.com>
2025-09-01 12:10:30 +0900

    LeetCode_202

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -1,31 +1,30 @@
 package leetcode;
 
 import java.io.IOException;
+import java.util.*;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0100-0199/0197.Rising%20Temperature/Solution.sql
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0200-0299/0202.Happy%20Number/Solution.java
 public class Main {
 
     public static void solution() throws IOException {
 
         // Testcase
-
-        //Weather table:
-        //+----+------------+-------------+
-        //| id | recordDate | temperature |
-        //+----+------------+-------------+
-        //| 1  | 2015-01-01 | 10          |
-        //| 2  | 2015-01-02 | 25          |
-        //| 3  | 2015-01-03 | 20          |
-        //| 4  | 2015-01-04 | 30          |
-        //+----+------------+-------------+
+        int n = 19;
 
         //--------------------------------------------------------------------------------------------------------------
 
-//        SELECT w1.id
-//                FROM
-//        Weather AS w1
-//        JOIN Weather AS w2
-//        ON DATEDIFF(w1.recordDate, w2.recordDate) = 1 AND w1.temperature > w2.temperature;
+        Set<Integer> vis = new HashSet<>();
+        while (n != 1 && !vis.contains(n)) {
+            vis.add(n);
+            int x = 0;
+            while (n != 0) {
+                // 각 자리수만큼 계산
+                x += (n % 10) * (n % 10);
+                n /= 10;
+            }
+            n = x;
+        }
+        //return n == 1;
 
         //--------------------------------------------------------------------------------------------------------------
     }
