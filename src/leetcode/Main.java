490b86c0e6d5b7474e826c4c35109abcc8b51712
BangJeongBin <bangjungbin@gmail.com>
2025-09-04 18:49:54 +0900

    LeetCode_217

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,36 +3,26 @@ package leetcode;
 import java.io.IOException;
 import java.util.*;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0200-0299/0206.Reverse%20Linked%20List/Solution.java
+// 참조 :
 public class Main {
 
     public static void solution() throws IOException {
 
-        /**
-         * Definition for singly-linked list.
-         * public class ListNode {
-         *     int val;
-         *     ListNode next;
-         *     ListNode() {}
-         *     ListNode(int val) { this.val = val; }
-         *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-         * }
-         */
-
         // Testcase
-        //ListNode head = {1,2,3,4,5};
+        int[] nums = {1, 2, 3, 1};
 
         //--------------------------------------------------------------------------------------------------------------
 
-//        ListNode dummy = new ListNode();
-//        ListNode curr = head;
-//        while (curr != null) {
-//            ListNode next = curr.next;
-//            curr.next = dummy.next;
-//            dummy.next = curr;
-//            curr = next;
-//        }
-//        return dummy.next;
+        Arrays.sort(nums); // 배열 정렬
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] == nums[i]) {
+                //return true;
+                System.out.println("true");
+            }
+        }
+        //return false;
+        System.out.println("false");
 
         //--------------------------------------------------------------------------------------------------------------
     }
