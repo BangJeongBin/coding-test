ca35e7a4210c861da6c6d66044468375644abb0c
BangJeongBin <bangjungbin@gmail.com>
2025-09-06 13:21:16 +0900

    LeetCode_222

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,28 +3,20 @@ package leetcode;
 import java.io.IOException;
 import java.util.*;
 
-// 참조 :
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0200-0299/0222.Count%20Complete%20Tree%20Nodes/Solution.java
 public class Main {
 
     public static void solution() throws IOException {
 
         // Testcase
-        int[] nums = {1, 2, 3, 1};
-        int k = 3;
+        //TreeNode root = [1,2,3,4,5,6]
 
         //--------------------------------------------------------------------------------------------------------------
 
-        Map<Integer, Integer> d = new HashMap<>();
-
-        for (int i = 0; i < nums.length; ++i) {
-            // 이전에 같은 값이 있었는지 확인
-            if (i - d.getOrDefault(nums[i], -1000000) <= k) {
-                //return true; // 인덱스 차이가 k 이하면 바로 true
-            }
-            // 현재 인덱스로 업데이트
-            d.put(nums[i], i);
-        }
-        //return false;
+//        if (root == null) {
+//            return 0;
+//        }
+        //return 1 + countNodes(root.left) + countNodes(root.right);
 
         //--------------------------------------------------------------------------------------------------------------
     }
