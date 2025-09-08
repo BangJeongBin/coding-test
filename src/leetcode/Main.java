cf62c6a4c553eb3cc6cf5506f52b638c08f66c4e
BangJeongBin <bangjungbin@gmail.com>
2025-09-08 22:46:18 +0900

    LeetCode_1317

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,54 +3,25 @@ package leetcode;
 import java.io.IOException;
 import java.util.*;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0200-0299/0225.Implement%20Stack%20using%20Queues/Solution.java
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/1300-1399/1317.Convert%20Integer%20to%20the%20Sum%20of%20Two%20No-Zero%20Integers/Solution.java
 public class Main {
 
     public static void solution() throws IOException {
 
         // Testcase
-        //input : ["MyStack","push","push","top","pop","empty"]
+        int n = 1010;
 
         //--------------------------------------------------------------------------------------------------------------
 
-        class MyStack {
-            private Deque<Integer> q1 = new ArrayDeque<>();
-            private Deque<Integer> q2 = new ArrayDeque<>();
+        for (int a = 1;; ++a) {
+            int b = n - a;
 
-            public MyStack() {
-            }
-
-            public void push(int x) {
-                q2.offer(x);
-                while (!q1.isEmpty()) {
-                    q2.offer(q1.poll());
-                }
-                Deque<Integer> q = q1;
-                q1 = q2;
-                q2 = q;
-            }
-
-            public int pop() {
-                return q1.poll();
-            }
-
-            public int top() {
-                return q1.peek();
-            }
-
-            public boolean empty() {
-                return q1.isEmpty();
+            // 문자열로 바꿔서 "0"이 포함되어 있는지 확인
+            if (!(a + "" + b).contains("0")) {
+                //return new int[] {a, b};
+                System.out.println(a + ", " + b);
             }
         }
-
-        /**
-         * Your MyStack object will be instantiated and called as such:
-         * MyStack obj = new MyStack();
-         * obj.push(x);
-         * int param_2 = obj.pop();
-         * int param_3 = obj.top();
-         * boolean param_4 = obj.empty();
-         */
 
         //--------------------------------------------------------------------------------------------------------------
     }
