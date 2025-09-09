0edb61960c0f210e130868484b488c543b0c826a
BangJeongBin <bangjungbin@gmail.com>
2025-09-09 17:15:42 +0900

    LeetCode_228

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,40 +3,31 @@ package leetcode;
 import java.io.IOException;
 import java.util.*;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0200-0299/0226.Invert%20Binary%20Tree/Solution.java
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0200-0299/0228.Summary%20Ranges/Solution.java
 public class Main {
 
     public static void solution() throws IOException {
 
-        /**
-         * Definition for a binary tree node.
-         * public class TreeNode {
-         *     int val;
-         *     TreeNode left;
-         *     TreeNode right;
-         *     TreeNode() {}
-         *     TreeNode(int val) { this.val = val; }
-         *     TreeNode(int val, TreeNode left, TreeNode right) {
-         *         this.val = val;
-         *         this.left = left;
-         *         this.right = right;
-         *     }
-         * }
-         */
-
         // Testcase
-        //TreeNode root = [4,2,7,1,3,6,9];
+        int[] nums = {0,2,3,4,6,8,9};
 
         //--------------------------------------------------------------------------------------------------------------
 
-//        if (root == null) {
-//            return null;
+//        public List<String> summaryRanges(int[] nums) {
+//            List<String> ans = new ArrayList<>();
+//            for (int i = 0, j, n = nums.length; i < n; i = j + 1) {
+//                j = i;
+//                while (j + 1 < n && nums[j + 1] == nums[j] + 1) {
+//                    ++j;
+//                }
+//                ans.add(f(nums, i, j));
+//            }
+//            return ans;
 //        }
-//        TreeNode l = invertTree(root.left);
-//        TreeNode r = invertTree(root.right);
-//        root.left = r;
-//        root.right = l;
-//        return root;
+//
+//        private String f(int[] nums, int i, int j) {
+//            return i == j ? nums[i] + "" : String.format("%d->%d", nums[i], nums[j]);
+//        }
 
         //--------------------------------------------------------------------------------------------------------------
     }
