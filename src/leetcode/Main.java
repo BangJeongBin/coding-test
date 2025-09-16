3b08e31e768c2d477558eead19e313bfd01ca2c4
BangJeongBin <bangjungbin@gmail.com>
2025-09-16 17:16:18 +0900

    LeetCode_2197

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,29 +3,40 @@ package leetcode;
 import java.io.IOException;
 import java.util.*;
 
-// 참조 :
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/2100-2199/2197.Replace%20Non-Coprime%20Numbers%20in%20Array/Solution.java
 public class Main {
 
     public static void solution() throws IOException {
 
         // Testcase
-        int num = 0;
+        int[] nums = {6, 4, 3, 2, 7, 6, 2};
 
         //--------------------------------------------------------------------------------------------------------------
 
-        while (true) {
-            String str = num + "";
-
-            if (str.length() == 1) {
-                //return num;
-                System.out.println("num ==> " + num);
-            } else {
-                num = 0;
-                for (char c : str.toCharArray()) {
-                    num += (c - '0');
-                }
-            }
-        }
+//        public List<Integer> replaceNonCoprimes(int[] nums) {
+//            List<Integer> stk = new ArrayList<>();
+//            for (int x : nums) {
+//                stk.add(x);
+//                while (stk.size() > 1) {
+//                    x = stk.get(stk.size() - 1);
+//                    int y = stk.get(stk.size() - 2);
+//                    int g = gcd(x, y);
+//                    if (g == 1) {
+//                        break;
+//                    }
+//                    stk.remove(stk.size() - 1);
+//                    stk.set(stk.size() - 1, (int) ((long) x * y / g));
+//                }
+//            }
+//            return stk;
+//        }
+//
+//        private int gcd(int a, int b) {
+//            if (b == 0) {
+//                return a;
+//            }
+//            return gcd(b, a % b);
+//        }
 
         //--------------------------------------------------------------------------------------------------------------
     }
