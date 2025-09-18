85c3e9fbf3c4f12eb5d06fcd5da9aef7f9930c8a
BangJeongBin <bangjungbin@gmail.com>
2025-09-18 15:38:42 +0900

    LeetCode_268

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -9,33 +9,24 @@ public class Main {
     public static void solution() throws IOException {
 
         // Testcase
-        int n = 50;
+        int[] nums = {9,6,4,2,3,5,7,0,1};
 
         //--------------------------------------------------------------------------------------------------------------
 
-        if (n == 0) { // n이 0인 경우
-            //return false;
-            System.out.println("False => 0");
+        List<Integer> list = new ArrayList<>();
+        for (int i : nums) {
+            list.add(i);
         }
+        Collections.sort(list);
 
-        while (n != 1) {
-            if (n % 5 == 0) { // 제시된 수 중 가장 큰 수인 5부터 검증하여 else if 문 통과
-                n /= 5;
-                System.out.println("5 => " + n);
-            } else if (n % 3 == 0) {
-                n /= 3;
-                System.out.println("3 => " + n);
-            } else if (n % 2 == 0) {
-                n /= 2;
-                System.out.println("2 => " + n);
-            } else {
-                //return false;
-                System.out.println("False => " + n);
-                break;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) != i) {
+                //return i;
+                System.out.println("result ==> " + i);
             }
         }
-        //return true;
-        System.out.println("True");
+        //return list.size();
+        System.out.println("result ==> " + list.size());
 
         //--------------------------------------------------------------------------------------------------------------
     }
