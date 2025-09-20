74815b2d60d26d7d6a12ac12c16fa4a4731c82a9
BangJeongBin <bangjungbin@gmail.com>
2025-09-20 22:22:19 +0900

    LeetCode_283

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,26 +3,34 @@ package leetcode;
 import java.io.IOException;
 import java.util.*;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0200-0299/0278.First%20Bad%20Version/Solution.java
+// 참조 :
 public class Main {
 
     public static void solution() throws IOException {
 
         // Testcase
-        int n = 5;
+        int[] nums = {0, 1, 0, 3, 12};
 
         //--------------------------------------------------------------------------------------------------------------
 
-//        int l = 1, r = n;
-//        while (l < r) {
-//            int mid = (l + r) >>> 1;
-//            if (isBadVersion(mid)) {
-//                r = mid;
-//            } else {
-//                l = mid + 1;
-//            }
-//        }
-//        return l;
+        int k = 0, j = 1;
+        int[] arr = new int[nums.length]; // spare variable
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) { // non-zero case
+                arr[k] = nums[i];
+                k++;
+            } else { // zero case
+                arr[nums.length - j] = nums[i];
+                j++;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            nums[i] = arr[i];
+        }
+
+        System.out.println(Arrays.toString(nums));
 
         //--------------------------------------------------------------------------------------------------------------
     }
