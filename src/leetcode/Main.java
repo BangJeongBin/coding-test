2b42b9e37a8e8b0dceb28a2a4ee1a71155783baa
BangJeongBin <bangjungbin@gmail.com>
2025-09-24 13:51:48 +0900

    LeetCode_303

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,17 +3,35 @@ package leetcode;
 import java.io.IOException;
 import java.util.*;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0200-0299/0292.Nim%20Game/Solution.java
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0300-0399/0303.Range%20Sum%20Query%20-%20Immutable/Solution.java
 public class Main {
 
     public static void solution() throws IOException {
 
         // Testcase
-        int n = 4;
+        //int[] nums = [[[-2,0,3,-5,2,-1]],[0,2],[2,5],[0,5]];
 
         //--------------------------------------------------------------------------------------------------------------
 
-        //return n % 4 != 0;
+//        private int[] s;
+//
+//        public NumArray(int[] nums) {
+//            int n = nums.length;
+//            s = new int[n + 1];
+//            for (int i = 0; i < n; ++i) {
+//                s[i + 1] = s[i] + nums[i];
+//            }
+//        }
+//
+//        public int sumRange(int left, int right) {
+//            return s[right + 1] - s[left];
+//        }
+
+        /**
+         * Your NumArray object will be instantiated and called as such:
+         * NumArray obj = new NumArray(nums);
+         * int param_1 = obj.sumRange(left,right);
+         */
 
         //--------------------------------------------------------------------------------------------------------------
     }
