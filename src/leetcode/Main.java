05a599893b4ad7bb5b5b94506819207a299e10d9
BangJeongBin <bangjungbin@gmail.com>
2025-09-25 20:12:10 +0900

    LeetCode_338

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,35 +3,21 @@ package leetcode;
 import java.io.IOException;
 import java.util.*;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0300-0399/0303.Range%20Sum%20Query%20-%20Immutable/Solution.java
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0300-0399/0338.Counting%20Bits/Solution.java
 public class Main {
 
     public static void solution() throws IOException {
 
         // Testcase
-        //int[] nums = [[[-2,0,3,-5,2,-1]],[0,2],[2,5],[0,5]];
+        int n = 2;
 
         //--------------------------------------------------------------------------------------------------------------
 
-//        private int[] s;
-//
-//        public NumArray(int[] nums) {
-//            int n = nums.length;
-//            s = new int[n + 1];
-//            for (int i = 0; i < n; ++i) {
-//                s[i + 1] = s[i] + nums[i];
-//            }
-//        }
-//
-//        public int sumRange(int left, int right) {
-//            return s[right + 1] - s[left];
-//        }
-
-        /**
-         * Your NumArray object will be instantiated and called as such:
-         * NumArray obj = new NumArray(nums);
-         * int param_1 = obj.sumRange(left,right);
-         */
+        int[] ans = new int[n + 1];
+        for (int i = 0; i <= n; ++i) {
+            ans[i] = Integer.bitCount(i);
+        }
+        //return ans;
 
         //--------------------------------------------------------------------------------------------------------------
     }
