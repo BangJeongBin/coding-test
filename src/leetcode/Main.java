a0fe146dc23b27a83889a12c0bfa78330a44c1b0
BangJeongBin <bangjungbin@gmail.com>
2025-09-26 12:51:27 +0900

    LeetCode_342

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,21 +3,17 @@ package leetcode;
 import java.io.IOException;
 import java.util.*;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0300-0399/0338.Counting%20Bits/Solution.java
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0300-0399/0342.Power%20of%20Four/Solution.java
 public class Main {
 
     public static void solution() throws IOException {
 
         // Testcase
-        int n = 2;
+        int n = 8;
 
         //--------------------------------------------------------------------------------------------------------------
 
-        int[] ans = new int[n + 1];
-        for (int i = 0; i <= n; ++i) {
-            ans[i] = Integer.bitCount(i);
-        }
-        //return ans;
+        //return n > 0 && (n & (n - 1)) == 0 && (n & 0xaaaaaaaa) == 0;
 
         //--------------------------------------------------------------------------------------------------------------
     }
