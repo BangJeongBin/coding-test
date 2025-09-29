550fbca171ba4e4cc394389bed194136c8b8840f
BangJeongBin <bangjungbin@gmail.com>
2025-09-29 14:57:07 +0900

    Update Main.java

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,24 +3,38 @@ package leetcode;
 import java.io.IOException;
 import java.util.*;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0900-0999/0976.Largest%20Perimeter%20Triangle/Solution.java
+// 참조 : 
 public class Main {
 
     public static void solution() throws IOException {
 
         // Testcase
-        int[] nums = {2, 1, 2};
+        String s = "IceCreAm"
 
         //--------------------------------------------------------------------------------------------------------------
 
-        Arrays.sort(nums);
-        for (int i = nums.length - 1; i >= 2; --i) {
-            int c = nums[i - 1] + nums[i - 2];
-            if (c > nums[i]) {
-                //return c + nums[i];
+        boolean[] vowels = new boolean[128];
+        for (char c : "aeiouAEIOU".toCharArray()) {
+            vowels[c] = true;
+        }
+        char[] cs = s.toCharArray();
+        int i = 0, j = cs.length - 1;
+        while (i < j) {
+            while (i < j && !vowels[cs[i]]) {
+                ++i;
+            }
+            while (i < j && !vowels[cs[j]]) {
+                --j;
+            }
+            if (i < j) {
+                char t = cs[i];
+                cs[i] = cs[j];
+            cs[j] = t;{
+                ++i;
+                --j;
             }
         }
-        //return 0;
+        return String.valueOf(cs);
 
         //--------------------------------------------------------------------------------------------------------------
     }
