56bd7ffec14d023a0279b2deaef4c0f1ef6498ed
BangJeongBin <bangjungbin@gmail.com>
2025-10-01 20:45:12 +0900

    LeetCode_350

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,36 +3,28 @@ package leetcode;
 import java.io.IOException;
 import java.util.*;
 
-// 참조 :
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0300-0399/0350.Intersection%20of%20Two%20Arrays%20II/Solution.java
 public class Main {
 
     public static void solution() throws IOException {
 
         // Testcase
-        int[] nums1 = {61,24,20,58,95,53,17,32,45,85,70,20,83,62,35,89,5,95,12,86,58,77,30,64,46,13,5,92,67,40,20,38,31,18,89,85,7,30,67,34,62,35,47,98,3,41,53,26,66,40,54,44,57,46,70,60,4,63,82,42,65,59,17,98,29,72,1,96,82,66,98,6,92,31,43,81,88,60,10,55,66,82,0,79,11,81};
-        int[] nums2 = {5,25,4,39,57,49,93,79,7,8,49,89,2,7,73,88,45,15,34,92,84,38,85,34,16,6,99,0,2,36,68,52,73,50,77,44,61,48};
+        int[] nums1 = {1,2,2,1};
+        int[] nums2 = {2};
 
         //--------------------------------------------------------------------------------------------------------------
 
-        List<Integer> list = new ArrayList<>();
-
-        for (int i = 0; i < nums1.length; i++) {
-            if (list.contains(nums1[i])) {
-                continue;
-            }
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums1[i] == nums2[j]) {
-                    list.add(nums1[i]);
-                    break;
-                }
+        int[] cnt = new int[1001];
+        for (int x : nums1) {
+            ++cnt[x];
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (int x : nums2) {
+            if (cnt[x]-- > 0) {
+                ans.add(x);
             }
         }
-        int[] result = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            result[i] = list.get(i);
-        }
-        //return result;
-        System.out.println(list);
+        //return ans.stream().mapToInt(Integer::intValue).toArray();
 
         //--------------------------------------------------------------------------------------------------------------
     }
