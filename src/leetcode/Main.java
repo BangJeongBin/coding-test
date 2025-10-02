56845b2c93b8fedbc3c3a09b8d786c34ac2a3fe9
BangJeongBin <bangjungbin@gmail.com>
2025-10-02 18:04:35 +0900

    LeetCode_3100

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,28 +3,26 @@ package leetcode;
 import java.io.IOException;
 import java.util.*;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0300-0399/0350.Intersection%20of%20Two%20Arrays%20II/Solution.java
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/3100-3199/3100.Water%20Bottles%20II/Solution.java
 public class Main {
 
     public static void solution() throws IOException {
 
         // Testcase
-        int[] nums1 = {1,2,2,1};
-        int[] nums2 = {2};
+        int numBottles = 13;
+        int numExchange = 6;
 
         //--------------------------------------------------------------------------------------------------------------
 
-        int[] cnt = new int[1001];
-        for (int x : nums1) {
-            ++cnt[x];
+        int ans = numBottles;
+        while (numBottles >= numExchange) {
+            numBottles -= numExchange;
+            ++numExchange;
+            ++ans;
+            ++numBottles;
         }
-        List<Integer> ans = new ArrayList<>();
-        for (int x : nums2) {
-            if (cnt[x]-- > 0) {
-                ans.add(x);
-            }
-        }
-        //return ans.stream().mapToInt(Integer::intValue).toArray();
+        System.out.println(ans);
+        //return ans;
 
         //--------------------------------------------------------------------------------------------------------------
     }
