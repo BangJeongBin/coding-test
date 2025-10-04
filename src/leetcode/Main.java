658a0b6b694a70969ac64d799defa7968eacee82
BangJeongBin <bangjungbin@gmail.com>
2025-10-04 20:36:35 +0900

    LeetCode_11

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,38 +3,25 @@ package leetcode;
 import java.io.IOException;
 import java.util.*;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0400-0499/0407.Trapping%20Rain%20Water%20II/Solution.java
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0000-0099/0011.Container%20With%20Most%20Water/Solution.java
 public class Main {
 
     public static void solution() throws IOException {
 
         // Testcase
-        int[][] heightMap = {{1, 4, 3, 1, 3, 2}, {3, 2, 1, 3, 2, 4}, {2, 3, 3, 2, 3, 1}};
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
 
         //--------------------------------------------------------------------------------------------------------------
 
-        int m = heightMap.length, n = heightMap[0].length;
-        boolean[][] vis = new boolean[m][n];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (i == 0 || i == m - 1 || j == 0 || j == n - 1) {
-                    pq.offer(new int[] {heightMap[i][j], i, j});
-                    vis[i][j] = true;
-                }
-            }
-        }
+        int l = 0, r = height.length - 1;
         int ans = 0;
-        int[] dirs = {-1, 0, 1, 0, -1};
-        while (!pq.isEmpty()) {
-            var p = pq.poll();
-            for (int k = 0; k < 4; ++k) {
-                int x = p[1] + dirs[k], y = p[2] + dirs[k + 1];
-                if (x >= 0 && x < m && y >= 0 && y < n && !vis[x][y]) {
-                    ans += Math.max(0, p[0] - heightMap[x][y]);
-                    vis[x][y] = true;
-                    pq.offer(new int[] {Math.max(p[0], heightMap[x][y]), x, y});
-                }
+        while (l < r) {
+            int t = Math.min(height[l], height[r]) * (r - l);
+            ans = Math.max(ans, t);
+            if (height[l] < height[r]) {
+                ++l;
+            } else {
+                --r;
             }
         }
         //return ans;
