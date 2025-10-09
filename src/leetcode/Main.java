3e0a4aa3c92451a1c32b9680ba8d1f5249867f2b
BangJeongBin <bangjungbin@gmail.com>
2025-10-09 14:13:32 +0900

    LeetCode_3494

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,36 +3,30 @@ package leetcode;
 import java.io.*;
 import java.util.*;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0300-0399/0374.Guess%20Number%20Higher%20or%20Lower/Solution.java
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/3400-3499/3494.Find%20the%20Minimum%20Amount%20of%20Time%20to%20Brew%20Potions/Solution.java
 public class Main {
 
     public static void solution() throws IOException {
 
         // Testcase
-        int n = 10;
-        //pick = 6
+        int[] skill = {1, 5, 2, 4};
+        int[] mana = {5, 1, 4, 2};
 
         //--------------------------------------------------------------------------------------------------------------
 
-        /**
-         * Forward declaration of guess API.
-         * @param  num   your guess
-         * @return 	     -1 if num is lower than the guess number
-         *			      1 if num is higher than the guess number
-         *               otherwise return 0
-         * int guess(int num);
-         */
-
-//        int left = 1, right = n;
-//        while (left < right) {
-//            int mid = (left + right) >>> 1;
-//            if (guess(mid) <= 0) {
-//                right = mid;
-//            } else {
-//                left = mid + 1;
-//            }
-//        }
-//        return left;
+        int n = skill.length;
+        long[] f = new long[n];
+        for (int x : mana) {
+            long tot = 0;
+            for (int i = 0; i < n; ++i) {
+                tot = Math.max(tot, f[i]) + skill[i] * x;
+            }
+            f[n - 1] = tot;
+            for (int i = n - 2; i >= 0; --i) {
+                f[i] = f[i + 1] - skill[i + 1] * x;
+            }
+        }
+        //return f[n - 1];
 
         //--------------------------------------------------------------------------------------------------------------
     }
