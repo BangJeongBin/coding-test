5f83574118f55b48e4ed18fec4cc248a8a2e2ac2
BangJeongBin <bangjungbin@gmail.com>
2025-10-10 13:23:44 +0900

    LeetCode_383

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,30 +3,28 @@ package leetcode;
 import java.io.*;
 import java.util.*;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/3400-3499/3494.Find%20the%20Minimum%20Amount%20of%20Time%20to%20Brew%20Potions/Solution.java
+// 참조 :
 public class Main {
 
     public static void solution() throws IOException {
 
         // Testcase
-        int[] skill = {1, 5, 2, 4};
-        int[] mana = {5, 1, 4, 2};
+        String ransomNote = "aa";
+        String magazine = "aab";
 
         //--------------------------------------------------------------------------------------------------------------
 
-        int n = skill.length;
-        long[] f = new long[n];
-        for (int x : mana) {
-            long tot = 0;
-            for (int i = 0; i < n; ++i) {
-                tot = Math.max(tot, f[i]) + skill[i] * x;
-            }
-            f[n - 1] = tot;
-            for (int i = n - 2; i >= 0; --i) {
-                f[i] = f[i + 1] - skill[i + 1] * x;
+        int[] cnt = new int[26];
+        for (int i = 0; i < magazine.length(); ++i) { // 알파벳마다 개수를 counting
+            ++cnt[magazine.charAt(i) - 'a'];
+        }
+        for (int i = 0; i < ransomNote.length(); ++i) {
+            if (--cnt[ransomNote.charAt(i) - 'a'] < 0) {
+                //return false;
+                System.out.println("false");
             }
         }
-        //return f[n - 1];
+        //return true;
 
         //--------------------------------------------------------------------------------------------------------------
     }
