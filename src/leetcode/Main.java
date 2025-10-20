eed835d089e067e14636c4431c8644bc53fe450b
BangJeongBin <bangjungbin@gmail.com>
2025-10-20 14:51:46 +0900

    LeetCode_392

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,42 +3,27 @@ package leetcode;
 import java.io.*;
 import java.util.*;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/1600-1699/1625.Lexicographically%20Smallest%20String%20After%20Applying%20Operations/Solution.java
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0300-0399/0392.Is%20Subsequence/Solution.java˚
 public class Main {
 
     public static void solution() throws IOException {
 
         // Testcase
-        String s = "5525";
-        int a = 9;
-        int b = 2;
+        String s = "acb";
+        String t = "ahbgdc";
 
         //--------------------------------------------------------------------------------------------------------------
 
-        Deque<String> q = new ArrayDeque<>();
-        q.offer(s);
-        Set<String> vis = new HashSet<>();
-        vis.add(s);
-        String ans = s;
-        int n = s.length();
-        while (!q.isEmpty()) {
-            s = q.poll();
-            if (ans.compareTo(s) > 0) {
-                ans = s;
+        int m = s.length(), n = t.length();
+        int i = 0, j = 0;
+        while (i < m && j < n) {
+            if (s.charAt(i) == t.charAt(j)) {
+                ++i;
             }
-            char[] cs = s.toCharArray();
-            for (int i = 1; i < n; i += 2) {
-                cs[i] = (char) (((cs[i] - '0' + a) % 10) + '0');
-            }
-            String t1 = String.valueOf(cs);
-            String t2 = s.substring(n - b) + s.substring(0, n - b);
-            for (String t : List.of(t1, t2)) {
-                if (vis.add(t)) {
-                    q.offer(t);
-                }
-            }
+            ++j;
         }
-        //return ans;
+        //return i == m;
+        System.out.println(i == m);
 
         //--------------------------------------------------------------------------------------------------------------
     }
