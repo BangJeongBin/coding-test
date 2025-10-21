5eda98f78b6d68248eab0e4df4f5a872cf75d9f1
BangJeongBin <bangjungbin@gmail.com>
2025-10-21 16:38:51 +0900

    LeetCode_401

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,27 +3,25 @@ package leetcode;
 import java.io.*;
 import java.util.*;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0300-0399/0392.Is%20Subsequence/Solution.java˚
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0400-0499/0401.Binary%20Watch/Solution.java
 public class Main {
 
     public static void solution() throws IOException {
 
         // Testcase
-        String s = "acb";
-        String t = "ahbgdc";
+        int turnedOn = 1;
 
         //--------------------------------------------------------------------------------------------------------------
 
-        int m = s.length(), n = t.length();
-        int i = 0, j = 0;
-        while (i < m && j < n) {
-            if (s.charAt(i) == t.charAt(j)) {
-                ++i;
+        List<String> ans = new ArrayList<>();
+        for (int i = 0; i < 12; ++i) {
+            for (int j = 0; j < 60; ++j) {
+                if (Integer.bitCount(i) + Integer.bitCount(j) == turnedOn) {
+                    ans.add(String.format("%d:%02d", i, j));
+                }
             }
-            ++j;
         }
-        //return i == m;
-        System.out.println(i == m);
+        //return ans;
 
         //--------------------------------------------------------------------------------------------------------------
     }
