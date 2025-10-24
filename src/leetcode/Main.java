78cc1b8319cd6ec332430459852d06a7fb7064dd
BangJeongBin <bangjungbin@gmail.com>
2025-10-24 13:39:10 +0900

    LeetCode_405

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,24 +3,30 @@ package leetcode;
 import java.io.*;
 import java.util.*;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/3400-3499/3461.Check%20If%20Digits%20Are%20Equal%20in%20String%20After%20Operations%20I/Solution.java
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0400-0499/0405.Convert%20a%20Number%20to%20Hexadecimal/Solution.java
 public class Main {
 
     public static void solution() throws IOException {
 
         // Testcase
-        String s = "3902";
+        int num = 26;
 
         //--------------------------------------------------------------------------------------------------------------
 
-        char[] t = s.toCharArray();
-        int n = t.length;
-        for (int k = n - 1; k > 1; --k) {
-            for (int i = 0; i < k; ++i) {
-                t[i] = (char) ((t[i] - '0' + t[i + 1] - '0') % 10 + '0');
-            }
+        if (num == 0) {
+            //return "0";
         }
-        //return t[0] == t[1];
+        StringBuilder sb = new StringBuilder();
+        while (num != 0) {
+            int x = num & 15;
+            if (x < 10) {
+                sb.append(x);
+            } else {
+                sb.append((char) (x - 10 + 'a'));
+            }
+            num >>>= 4;
+        }
+        //return sb.reverse().toString();
 
         //--------------------------------------------------------------------------------------------------------------
     }
