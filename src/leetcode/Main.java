7f65fd546a8f6baabcba0570a8eae7234aaec038
BangJeongBin <bangjungbin@gmail.com>
2025-10-27 16:08:34 +0900

    LeetCode_412

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -9,21 +9,27 @@ public class Main {
     public static void solution() throws IOException {
 
         // Testcase
-        String s = "AAAAAA";
+        int n = 15;
 
         //--------------------------------------------------------------------------------------------------------------
 
-        int[] cnt = new int[128]; // ascii 코드 개수
-        int n = s.length();
-        for (int i = 0; i < n; ++i) {
-            ++cnt[s.charAt(i)];
+        List<String> list = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                list.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                list.add("Fizz");
+            } else if (i % 5 == 0) {
+                list.add("Buzz");
+            } else {
+                list.add("" + i);
+            }
         }
-        int ans = 0;
-        for (int v : cnt) {
-            ans += v / 2 * 2; // 짝수인 경우만 필터
+        //return list;
+        for (String s : list) {
+            System.out.println("s = " + s);
         }
-        ans += ans < n ? 1 : 0; // 결과값 홀수 만들기
-        //return ans;
 
         //--------------------------------------------------------------------------------------------------------------
     }
