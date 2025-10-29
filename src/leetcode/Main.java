a35847215052142f252c1738d6e2bd1bae7375fd
BangJeongBin <bangjungbin@gmail.com>
2025-10-29 09:01:50 +0900

    LeetCode_3354

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -3,46 +3,28 @@ package leetcode;
 import java.io.*;
 import java.util.*;
 
-// 참조 : https://github.com/doocs/leetcode/blob/main/solution/0000-0099/0003.Longest%20Substring%20Without%20Repeating%20Characters/Solution.java
+// 참조 : https://github.com/doocs/leetcode/blob/main/solution/3300-3399/3354.Make%20Array%20Elements%20Equal%20to%20Zero/Solution.java
 public class Main {
 
     public static void solution() throws IOException {
 
         // Testcase
-        String s = "dvdf";
+        int[] nums = {1, 0, 2, 0, 3};
 
         //--------------------------------------------------------------------------------------------------------------
 
-        int[] alpha = new int[128];
-        int result = 0;
-        int length = s.length();
-
-        for (int i = 0, j = 0; j < length; j++) {
-            char c = s.charAt(j);
-            ++alpha[c];
-            while (alpha[c] > 1) {
-                --alpha[s.charAt(i++)];
+        int s = Arrays.stream(nums).sum();
+        int ans = 0, l = 0;
+        for (int x : nums) {
+            if (x != 0) {
+                l += x;
+            } else if (l * 2 == s) {
+                ans += 2;
+            } else if (Math.abs(l * 2 - s) <= 1) {
+                ++ans;
             }
-            result = Math.max(result, j - i + 1);
         }
-        //return result;
-        System.out.println("result ==> " + result);
-
-        //--------------------------------------------------------------------------------------------------------------
-
-        HashSet<Character> hs = new HashSet<>();
-        int l = 0;
-        int maxlen = 0;
-        for (int r = 0; r < s.length(); r++) {
-            char ch = s.charAt(r);
-            while (hs.contains(ch)) {
-                hs.remove(s.charAt(l));
-                l++;
-            }
-            hs.add(ch);
-            maxlen = Math.max(maxlen, r - l + 1);
-        }
-        //return maxlen;
+        //return ans;
 
         //--------------------------------------------------------------------------------------------------------------
     }
