f9df299ec080f86ac0e196f8508a88a4b8944392
BangJeongBin <bangjungbin@gmail.com>
2025-11-05 15:17:32 +0900

    Baekjoon_2609

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -3,20 +3,63 @@ package baekjoon;
 import java.io.*;
 import java.util.*;
 
-// 참조 :
+// 참조 : https://www.acmicpc.net/source/82518947
 public class Main {
 
     public static void solution() throws IOException {
-//        n = int(input()) # 입력받을 점수의 개수
-//        case = list(map(int, input().split())) # 입력받은 점수 리스트
-//                m = max(case) # 최대값
-//                sumScore = 0 # 새로 계산한 점수의 값
-//
-//        for i in case:
-//        sumScore += (i / m * 100)
-//
-//        print(sumScore / n)
 
+//        nList = list(map(int, input().split())) # 입력 받은 수
+//        maxNum = []
+//        minNum = []
+//
+//        for i in range(1, 11) :
+//            minNum.append(nList[0] * i)
+//
+//        for i in range(1, (nList[0] + 1)):
+//            if nList[0] % i == 0 :
+//                maxNum.append(i)
+//
+//        maxNum.reverse() # 약수 뒤짚기
+//
+//        for i in maxNum :
+//            if nList[1] % i == 0:
+//                print(i)
+//                break
+//
+//        mul1, mul2 = 1, 1
+//
+//        while True :
+//            if (nList[0] * mul2) == (nList[1] * mul1) :
+//                print(nList[0] * mul2)
+//                break
+//
+//            if (nList[0] * mul2) > (nList[1] * mul1) :
+//                mul1 += 1
+//            else :
+//                mul2 += 1
+
+
+
+
+
+        // 다른 풀이
+//        def myGCD(a, b):
+//            if a % b == 0:
+//                return b
+//
+//            return myGCD(b, a % b)
+//
+//        a, b = map(int, input().split())
+//
+//        if a < b:
+//            tmp = a
+//            a = b
+//            b = tmp
+//
+//        gcd = myGCD(a,b)
+//
+//        print(int(gcd))
+//        print(int(a*b/gcd))
     }
 
 
