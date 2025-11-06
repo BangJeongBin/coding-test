eef1506d2113348e5ce741bc5e8cc149c4959e16
BangJeongBin <bangjungbin@gmail.com>
2025-11-06 15:38:13 +0900

    Baekjoon_2775

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -3,63 +3,22 @@ package baekjoon;
 import java.io.*;
 import java.util.*;
 
-// 참조 : https://www.acmicpc.net/source/82518947
+// 참조 : https://ooyoung.tistory.com/89
 public class Main {
 
     public static void solution() throws IOException {
 
-//        nList = list(map(int, input().split())) # 입력 받은 수
-//        maxNum = []
-//        minNum = []
-//
-//        for i in range(1, 11) :
-//            minNum.append(nList[0] * i)
-//
-//        for i in range(1, (nList[0] + 1)):
-//            if nList[0] % i == 0 :
-//                maxNum.append(i)
-//
-//        maxNum.reverse() # 약수 뒤짚기
-//
-//        for i in maxNum :
-//            if nList[1] % i == 0:
-//                print(i)
-//                break
-//
-//        mul1, mul2 = 1, 1
-//
-//        while True :
-//            if (nList[0] * mul2) == (nList[1] * mul1) :
-//                print(nList[0] * mul2)
-//                break
-//
-//            if (nList[0] * mul2) > (nList[1] * mul1) :
-//                mul1 += 1
-//            else :
-//                mul2 += 1
+    //t = int(input())
+    //
+    //for _ in range(t):
+    //    floor = int(input())  # 층
+    //    num = int(input())  # 호
+    //    f0 = [x for x in range(1, num+1)]  # 0층 리스트
+    //    for k in range(floor):  # 층 수 만큼 반복
+    //        for i in range(1, num):  # 1 ~ n-1까지 (인덱스로 사용)
+    //            f0[i] += f0[i-1]  # 층별 각 호실의 사람 수를 변경
+    //    print(f0[-1])  # 가장 마지막 수 출력
 
-
-
-
-
-        // 다른 풀이
-//        def myGCD(a, b):
-//            if a % b == 0:
-//                return b
-//
-//            return myGCD(b, a % b)
-//
-//        a, b = map(int, input().split())
-//
-//        if a < b:
-//            tmp = a
-//            a = b
-//            b = tmp
-//
-//        gcd = myGCD(a,b)
-//
-//        print(int(gcd))
-//        print(int(a*b/gcd))
     }
 
 
