086614c4e8f5572fa0f7bef12d1ca7b73ce16e8d
BangJeongBin <bangjungbin@gmail.com>
2025-11-09 14:41:01 +0900

    Baekjoon_10989

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -3,18 +3,34 @@ package baekjoon;
 import java.io.*;
 import java.util.*;
 
-// 참조 : https://happyhyxndy.tistory.com/61
+// 참조 : https://velog.io/@klaus/%ED%8C%8C%EC%9D%B4%EC%8D%ACPython-%EB%B0%B1%EC%A4%80-10989%EB%B2%88-%EC%88%98-%EC%A0%95%EB%A0%AC%ED%95%98%EA%B8%B0-3
 public class Main {
 
     public static void solution() throws IOException {
 
-//        A, B, V = map(int, input().split())
+//        import sys
 //
-//        x = (V - B) / (A - B)
-//        if x == int(x):
-//            print(int(x)) # 값이 정수인 경우
-//        else:
-//            print(int(x) + 1) # 값이 실수인 경우
+//        input = sys.stdin.readline
+//
+//        # 1. N 입력
+//        N = int(input())
+//
+//        # 2. 10001 크기의 리스트 정의
+//        count = [0] * 10001
+//
+//        # 3. N개의 정수를 리스트의 인덱스로 접근하여 개수 세기
+//        for _ in range(N):
+//            num = int(input())
+//            count[num] += 1
+//
+//        # 4. 리스트에 접근해 그 개수만큼 인덱스 출력
+//        for i in range(10001):
+//            # count[i]이 1 이상이라면
+//            if count[i] != 0:
+//                # 그 값만큼 출력
+//                for j in range(count[i]):
+//                    print(i)
+//
 
     }
 
