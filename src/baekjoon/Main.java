6b5de70c39164fc3c0eae12589f76bea716eeeb4
BangJeongBin <bangjungbin@gmail.com>
2025-11-10 15:57:57 +0900

    Baekjoon_14626

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -3,34 +3,28 @@ package baekjoon;
 import java.io.*;
 import java.util.*;
 
-// 참조 : https://velog.io/@klaus/%ED%8C%8C%EC%9D%B4%EC%8D%ACPython-%EB%B0%B1%EC%A4%80-10989%EB%B2%88-%EC%88%98-%EC%A0%95%EB%A0%AC%ED%95%98%EA%B8%B0-3
+// 참조 : https://codespeed.tistory.com/465
 public class Main {
 
     public static void solution() throws IOException {
 
-//        import sys
+//        isbn = input()
+//        result = 0
+//        is_even = False
+//        for i in range(13):
+//            if isbn[i] == '*':
+//                if i % 2 != 0:
+//                    is_even = True
+//                continue
+//            result += int(isbn[i]) * (1 if i % 2 == 0 else 3)
 //
-//        input = sys.stdin.readline
-//
-//        # 1. N 입력
-//        N = int(input())
-//
-//        # 2. 10001 크기의 리스트 정의
-//        count = [0] * 10001
-//
-//        # 3. N개의 정수를 리스트의 인덱스로 접근하여 개수 세기
-//        for _ in range(N):
-//            num = int(input())
-//            count[num] += 1
-//
-//        # 4. 리스트에 접근해 그 개수만큼 인덱스 출력
-//        for i in range(10001):
-//            # count[i]이 1 이상이라면
-//            if count[i] != 0:
-//                # 그 값만큼 출력
-//                for j in range(count[i]):
+//        if is_even == True:
+//            for i in range(10):
+//                if (result + (i * 3)) % 10 == 0:
 //                    print(i)
-//
+//                    break
+//        else:
+//            print(10 - result % 10)
 
     }
 
