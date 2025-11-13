f571676c06018fe412e2d4d5fe6fe801631daa78
BangJeongBin <bangjungbin@gmail.com>
2025-11-13 15:10:35 +0900

    Baekjoon_28702

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -3,21 +3,33 @@ package baekjoon;
 import java.io.*;
 import java.util.*;
 
-// 참조 : https://develop247.tistory.com/356
+// 참조 :
 public class Main {
 
     public static void solution() throws IOException {
 
-//            import sys
-//            import math
+//        import sys
 //
-//            input = sys.stdin.readline
+//        input = sys.stdin.readline
 //
-//            # 입력
-//            n, k = map(int, input().split())
+//        result = 0
+//        n = []
 //
-//            # 이항계수 공식
-//            print(math.factorial(n) // (math.factorial(k) * math.factorial(n-k)))
+//        for i in range(3):
+//            n.append(input())
+//
+//        for i in range(3):
+//            if 'z' not in n[i]: # 숫자인 경우
+//                result = int(n[i]) + (3 - i) # 출력할 숫자 할당
+//
+//        if result % 3 == 0 and result % 5 == 0:
+//            print('FizzBuzz')
+//        elif result % 5 == 0:
+//            print('Buzz')
+//        elif result % 3 == 0:
+//            print('Fizz')
+//        else:
+//            print(result)
 
     }
 
