36483bbc9d2ef3cd1f968356c07ea36538f68e0f
BangJeongBin <bangjungbin@gmail.com>
2025-11-23 20:31:58 +0900

    Baekjoon_7568

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -3,27 +3,26 @@ package baekjoon;
 import java.io.*;
 import java.util.*;
 
-// 참조 : https://auyeol-coding.tistory.com/62
+// 참조 : https://mxxcode.tistory.com/10
 public class Main {
 
     public static void solution() throws IOException {
 
 //        import sys
 //
-//        K = int(sys.stdin.readline())    # 첫 번째 줄에 정수 K가 주어진다.
+//        n = int(input())
+//        people = []
 //
-//        ST = []             # 빈 스택 생성
-//        sum_v = 0           # 합을 구할 sum_v 변수 초기화
+//        for _ in range(n):
+//            people.append(list(map(int, input().split())))
 //
-//        for _ in range(K):  # K개의 줄에 정수가 1개씩 주어진다.
-//            num = int(sys.stdin.readline())  # 값 입력받기
+//        for i in people:
+//            ranking = 1
+//            for j in people:
+//                if i[0] < j[0] and i[1] < j[1]:
+//                    ranking += 1
 //
-//            if ST and num == 0: # 스택에 값이 존재하고, 입력받은 값이 0이면
-//                ST.pop()        # 스택에서 pop()
-//            else:
-//                ST.append(num)  # 아닌경우 스택에 append()
-//
-//        print(sum(ST))          # 스택의 합을 출력
+//            print(ranking, end=" ")
 
     }
 
