b7431418900ee954dcd9f1189516290389c962ac
BangJeongBin <bangjungbin@gmail.com>
2025-11-24 17:09:37 +0900

    Baekjoon_11651

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -9,12 +9,12 @@ public class Main {
     public static void solution() throws IOException {
 
 //        import sys
-//        n=int(sys.stdin.readline())
+//        n=int(input())
 //        li=[]
 //        for i in range(n):
-//            a, b = map(int, sys.stdin.readline().split())
-//            li.append([a, b])
-//        li.sort()
+//            a, b = map(int,sys.stdin.readline().split())
+//            li.append([a,b])
+//        li.sort(key=lambda x: (x[1], x[0]))
 //        for i in li:
 //            print(i[0], i[1])
 
