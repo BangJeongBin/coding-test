e87c03880cc3876579fc4da41d20aacfcce6d38e
BangJeongBin <bangjungbin@gmail.com>
2025-11-26 16:23:07 +0900

    Baekjoon_2164

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -3,44 +3,24 @@ package baekjoon;
 import java.io.*;
 import java.util.*;
 
-// 참조 : https://mxxcode.tistory.com/84
+// 참조 : https://interrobang.tistory.com/371
 public class Main {
 
     public static void solution() throws IOException {
 
 //        import sys
+//        from collections import deque
+//
 //        input = sys.stdin.readline
 //
-//        # 이분탐색 메서드
-//        def binary_search(target, nList):
-//            start = 0 # 증감값
-//            end = n - 1 # 고정값
+//        n = int(input())
+//        card = deque([i for i in range(1, n + 1)])
 //
-//            while start <= end:
-//                mid = (start + end) // 2 # 중간값 설정
+//        while len(card) != 1:
+//            card.popleft()
+//            card.append(card.popleft())
 //
-//                if nList[mid] == target:
-//                    return True
-//                elif nList[mid] < target:
-//                    start = mid + 1 # 왼쪽 구간을 버리고 오른쪽으로 이동
-//                else:
-//                    end = mid - 1 # 오른쪽 구간을 버리고 완쪽으로 이동
-//
-//            return False
-//
-//
-//        n = int(input()) # n값 할당
-//        nList = sorted(list(map(int, input().split()))) # n개의 리스트 초기화 - 정렬작업 진행
-//
-//        m = int(input()) # m값 할당
-//        mList = list(map(int, input().split())) # m개의 리스트 초기화
-//
-//        for i in mList:
-//            if binary_search(i, nList):
-//                print(1)
-//            else:
-//                print(0)
-
+//        print(card[0])
 
     }
 
