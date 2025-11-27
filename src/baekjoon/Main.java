ed4dbe7a87b336ccb46f0e6384ebfd0a06e5c456
BangJeongBin <bangjungbin@gmail.com>
2025-11-27 16:00:30 +0900

    Baekjoon_2839

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -3,24 +3,23 @@ package baekjoon;
 import java.io.*;
 import java.util.*;
 
-// 참조 : https://interrobang.tistory.com/371
+// 참조 :
 public class Main {
 
     public static void solution() throws IOException {
 
 //        import sys
-//        from collections import deque
-//
 //        input = sys.stdin.readline
 //
 //        n = int(input())
-//        card = deque([i for i in range(1, n + 1)])
 //
-//        while len(card) != 1:
-//            card.popleft()
-//            card.append(card.popleft())
-//
-//        print(card[0])
+//        for five in range(n // 5, -1, -1):   # 5kg 봉지 수를 최대부터 0까지 감소
+//            rem = n - 5 * five               # 남은 무게
+//            if rem % 3 == 0:
+//                print(five + rem // 3)
+//                break
+//        else:
+//            print(-1)
 
     }
 
