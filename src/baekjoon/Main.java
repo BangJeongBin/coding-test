ef67199cdfb9ceeb86e9bc2f6bffc3f326842087
BangJeongBin <bangjungbin@gmail.com>
2025-11-28 14:41:20 +0900

    Baekjoon_4949

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -3,23 +3,38 @@ package baekjoon;
 import java.io.*;
 import java.util.*;
 
-// 참조 :
+// 참조 : https://dduniverse.tistory.com/entry/%EB%B0%B1%EC%A4%80-4949%EB%B2%88-%EA%B7%A0%ED%98%95%EC%9E%A1%ED%9E%8C-%EC%84%B8%EC%83%81-%ED%8C%8C%EC%9D%B4%EC%8D%AC-Python
 public class Main {
 
     public static void solution() throws IOException {
 
-//        import sys
-//        input = sys.stdin.readline
+//        while(True):
+//            word=input()
 //
-//        n = int(input())
-//
-//        for five in range(n // 5, -1, -1):   # 5kg 봉지 수를 최대부터 0까지 감소
-//            rem = n - 5 * five               # 남은 무게
-//            if rem % 3 == 0:
-//                print(five + rem // 3)
+//            stack=[] # 괄호를 추가할 리스트 stack
+//            if word=='.': # . 이 들어오면 종료
 //                break
-//        else:
-//            print(-1)
+//
+//            for w in word:
+//                if w=='(' or w=='[': # 여는 괄호가 오면 stack에 추가
+//                    stack.append(w)
+//                elif w==')': # ) 인 경우
+//                    if len(stack)!=0 and stack[-1]=='(': # stack이 비어있지 않고, 마지막 요소가 ( 이면 pop
+//                        stack.pop()
+//                    else: # stack이 비어있거나 짝이 맞지 않으면 stack에 )을 추가하고 break
+//                        stack.append(w)
+//                        break
+//                elif w==']': # ] 인 경우
+//                    if len(stack)!=0 and stack[-1]=='[': # stack이 비어있지 않고, 마지막 요소가 [ 이면 pop
+//                        stack.pop()
+//                    else: # stack이 비어있거나 짝이 맞지 않으면 stack에 ]을 추가하고 break
+//                        stack.append(w)
+//                        break
+//
+//            if len(stack)==0: # stack이 비어있으면 모든 괄호들의 균형이 맞으므로 Yes 출력
+//                print('yes')
+//            else: # stack이 비어있지 않으면 괄호들의 균형이 맞지 않는 것이므로 No 출력
+//                print('no')
 
     }
 
