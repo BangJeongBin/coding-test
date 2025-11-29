968a63529a11fae9337973f338f68dad5af2f5b1
BangJeongBin <bangjungbin@gmail.com>
2025-11-29 13:19:46 +0900

    Baekjoon_9012

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -3,38 +3,30 @@ package baekjoon;
 import java.io.*;
 import java.util.*;
 
-// 참조 : https://dduniverse.tistory.com/entry/%EB%B0%B1%EC%A4%80-4949%EB%B2%88-%EA%B7%A0%ED%98%95%EC%9E%A1%ED%9E%8C-%EC%84%B8%EC%83%81-%ED%8C%8C%EC%9D%B4%EC%8D%AC-Python
+// 참조 : https://omins.tistory.com/34
 public class Main {
 
     public static void solution() throws IOException {
 
-//        while(True):
-//            word=input()
+//        t = int(input())
+//        for _ in range(t):
+//            stk = []
+//            s = input()
+//            isVPS = True
 //
-//            stack=[] # 괄호를 추가할 리스트 stack
-//            if word=='.': # . 이 들어오면 종료
-//                break
-//
-//            for w in word:
-//                if w=='(' or w=='[': # 여는 괄호가 오면 stack에 추가
-//                    stack.append(w)
-//                elif w==')': # ) 인 경우
-//                    if len(stack)!=0 and stack[-1]=='(': # stack이 비어있지 않고, 마지막 요소가 ( 이면 pop
-//                        stack.pop()
-//                    else: # stack이 비어있거나 짝이 맞지 않으면 stack에 )을 추가하고 break
-//                        stack.append(w)
+//            for ch in s:
+//                if ch == '(':
+//                    stk.append('(')
+//                if ch == ')':
+//                    if stk:
+//                        stk.pop()
+//                    elif not stk:
+//                        isVPS = False
 //                        break
-//                elif w==']': # ] 인 경우
-//                    if len(stack)!=0 and stack[-1]=='[': # stack이 비어있지 않고, 마지막 요소가 [ 이면 pop
-//                        stack.pop()
-//                    else: # stack이 비어있거나 짝이 맞지 않으면 stack에 ]을 추가하고 break
-//                        stack.append(w)
-//                        break
-//
-//            if len(stack)==0: # stack이 비어있으면 모든 괄호들의 균형이 맞으므로 Yes 출력
-//                print('yes')
-//            else: # stack이 비어있지 않으면 괄호들의 균형이 맞지 않는 것이므로 No 출력
-//                print('no')
+//            if not stk and isVPS:
+//                print('YES')
+//            elif stk or not isVPS:
+//                print('NO')
 
     }
 
