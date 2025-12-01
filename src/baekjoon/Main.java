2d4c82e4bffa3ec86c7a200acddf2ecdbaaf685c
BangJeongBin <bangjungbin@gmail.com>
2025-12-01 14:12:55 +0900

    Baekjoon_10828

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -3,7 +3,7 @@ package baekjoon;
 import java.io.*;
 import java.util.*;
 
-// 참조 : https://divheer.tistory.com/363
+// 참조 :
 public class Main {
 
     public static void solution() throws IOException {
@@ -11,23 +11,68 @@ public class Main {
 //        import sys
 //        input = sys.stdin.readline
 //
+//        stack = []
+//
+//        def push(x):
+//            stack.append(x)
+//
+//        def pop():
+//            if stack:
+//                print(stack[len(stack) - 1])
+//                del stack[len(stack) - 1]
+//            else:
+//                print(-1)
+//
+//        def size():
+//            print(len(stack))
+//
+//        def empty():
+//            if stack:
+//                print(0)
+//            else:
+//                print(1)
+//
+//        def top():
+//            if stack:
+//                print(stack[len(stack) - 1])
+//            else:
+//                print(-1)
+//
+//        for _ in range(int(input())):
+//            command = list(input().split())
+//            if command[0] == 'pop':
+//                pop()
+//            elif command[0] == 'size':
+//                size()
+//            elif command[0] == 'empty':
+//                empty()
+//            elif command[0] == 'top':
+//                top()
+//            else:
+//                push(command[1])
+
+// -----------------------------------------------------------------------------------------------
+
+//        import sys
+//        input = sys.stdin.readline
+//
 //        n = int(input())
-//        my_list = map(int, input().split())
-//        m = int(input())
-//        card_list = map(int, input().split())
 //
-//        dic = {}
-//        for i in my_list:
-//            if i in dic:
-//                dic[i] += 1
-//            else:
-//                dic[i] = 1 # 해당 값 초기화
+//        arr = []
 //
-//        for i in card_list:
-//            if i in dic:
-//                print(dic[i], end=" ")
-//            else:
-//                print(0, end=" ")
+//        for _ in range(n):
+//            input_list = input().split()
+//            if input_list[0] == "push":
+//                arr.append(input_list[1])
+//            elif input_list[0] == "pop":
+//                print(arr.pop() if arr else -1) # 내장 함수 사용
+//            elif input_list[0] == "size":
+//                print(len(arr))
+//            elif input_list[0] == "empty":
+//                print(1 if not arr else 0)
+//            elif input_list[0] == "top":
+//                print(arr[-1] if arr else -1)
+
 
     }
 
