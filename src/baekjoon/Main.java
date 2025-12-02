ee316144a3e1bab7887fc11f2371212b5e9c6f7c
BangJeongBin <bangjungbin@gmail.com>
2025-12-02 15:08:03 +0900

    Baekjoon_10845

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -3,75 +3,38 @@ package baekjoon;
 import java.io.*;
 import java.util.*;
 
-// 참조 :
+// 참조 : https://seokii.tistory.com/115
 public class Main {
 
     public static void solution() throws IOException {
 
+//        from collections import deque
 //        import sys
+//
 //        input = sys.stdin.readline
-//
-//        stack = []
-//
-//        def push(x):
-//            stack.append(x)
-//
-//        def pop():
-//            if stack:
-//                print(stack[len(stack) - 1])
-//                del stack[len(stack) - 1]
-//            else:
-//                print(-1)
-//
-//        def size():
-//            print(len(stack))
-//
-//        def empty():
-//            if stack:
-//                print(0)
-//            else:
-//                print(1)
-//
-//        def top():
-//            if stack:
-//                print(stack[len(stack) - 1])
-//            else:
-//                print(-1)
-//
-//        for _ in range(int(input())):
-//            command = list(input().split())
-//            if command[0] == 'pop':
-//                pop()
-//            elif command[0] == 'size':
-//                size()
-//            elif command[0] == 'empty':
-//                empty()
-//            elif command[0] == 'top':
-//                top()
-//            else:
-//                push(command[1])
-
-// -----------------------------------------------------------------------------------------------
-
-//        import sys
-//        input = sys.stdin.readline
-//
 //        n = int(input())
-//
-//        arr = []
+//        dq = deque()
 //
 //        for _ in range(n):
-//            input_list = input().split()
-//            if input_list[0] == "push":
-//                arr.append(input_list[1])
-//            elif input_list[0] == "pop":
-//                print(arr.pop() if arr else -1) # 내장 함수 사용
-//            elif input_list[0] == "size":
-//                print(len(arr))
-//            elif input_list[0] == "empty":
-//                print(1 if not arr else 0)
-//            elif input_list[0] == "top":
-//                print(arr[-1] if arr else -1)
+//            command = list(input().split())
+//            if command[0] == 'push':
+//                dq.append(command[1])
+//            elif command[0] == 'pop':
+//                if len(dq) > 0 :
+//                    tmp = dq.popleft()
+//                    print(tmp)
+//                else : print(-1)
+//            elif command[0] == 'size':
+//                print(len(dq))
+//            elif command[0] == 'empty':
+//                if len(dq) > 0: print(0)
+//                else : print(1)
+//            elif command[0] == 'front':
+//                if len(dq) > 0: print(dq[0])
+//                else : print(-1)
+//            elif command[0] == 'back':
+//                if len(dq) > 0: print(dq[-1])
+//                else: print(-1)
 
 
     }
