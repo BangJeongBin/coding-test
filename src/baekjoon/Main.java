28a0e31d5c67d1ee2fe0cdecd501cdbeb6fe3c77
BangJeongBin <bangjungbin@gmail.com>
2025-12-08 14:47:18 +0900

    Baekjoon_1966

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -3,27 +3,33 @@ package baekjoon;
 import java.io.*;
 import java.util.*;
 
-// 참조 :
+// 참조 : https://lemon27.tistory.com/56
 public class Main {
 
     public static void solution() throws IOException {
 
 //        import sys
+//        from collections import deque
+//
 //        input = sys.stdin.readline
-//
-//        m, n = map(int, input().split())
-//
-//        def is_prime(x):
-//            if x < 2:
-//                return False
-//            for i in range(2, int(x**0.5) + 1): # int(x**0.5) --> x의 제곱근
-//                if x % i == 0:
-//                    return False
-//            return True
-//
-//        for i in range(m, n + 1):
-//            if is_prime(i):
-//                print(i)
+//        T = int(input())
+//        for _ in range(T):
+//            n, m = map(int, input().split())
+//            priority = list(map(int, input().split()))
+//            q = deque(priority)
+//            cnt = 0
+//            priority.sort()
+//            while True:
+//                if q[0] == priority[-1]: # 큐의 가장 앞에 있는 원소가 중요도가 가장 높은 문서일 때
+//                    cnt += 1
+//                    q.popleft()
+//                    priority.pop()
+//                    if m == 0:
+//                        break
+//                else:
+//                    q.append(q.popleft())
+//                m = len(q) - 1 if m == 0 else m - 1
+//            print(cnt)
 
     }
 
