c2b0c47b9288062ebd8ec956939c3f8df18db59c
BangJeongBin <bangjungbin@gmail.com>
2025-12-27 17:09:57 +0900

    Baekjoon_1764

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -3,53 +3,27 @@ package baekjoon;
 import java.io.*;
 import java.util.*;
 
-// 참조 : https://thisismi.tistory.com/entry/%EB%B0%B1%EC%A4%80-2108%EB%B2%88-%ED%86%B5%EA%B3%84%ED%95%99-%ED%8C%8C%EC%9D%B4%EC%8D%AC-%EC%A0%95%EB%8B%B5-%EC%BD%94%EB%93%9C
+// 참조 : https://night-knight.tistory.com/entry/%EB%B0%B1%EC%A4%801764-%EB%93%A3%EB%B3%B4%EC%9E%A1-python-%ED%8C%8C%EC%9D%B4%EC%8D%AC
 public class Main {
 
     public static void solution() throws IOException {
 
-//        import sys
-//
-//        input = sys.stdin.readline
-//
-//        n = int(input())
-//        data = []
-//
-//        _sum = 0
-//        count = dict()
+//        n,m = map(int,input().split())
+//        a=set()
+//        b=set()
+//        result =[]
 //        for _ in range(n):
-//            x = int(input())
-//            data.append(x)
+//            a.add(input())
+//        for _ in range(m):
+//            b.add(input())
 //
-//            _sum += x
-//
-//            if x not in count:
-//                count[x] = 1
-//            else:
-//                count[x] += 1
-//
-//        data.sort()
-//
-//        # 산술평균
-//        print(round(_sum/n))
-//
-//        # 중앙값
-//        print(data[n//2])
-//
-//        # 최빈값
-//        freq = max(count.values())
-//        numbers = []
-//        for key, value in count.items():
-//            if value == freq:
-//                numbers.append(key)
-//
-//        if len(numbers) == 1:
-//            print(numbers[0])
-//        else:
-//            print(sorted(numbers)[1])
-//
-//        # 범위
-//        print(data[-1] - data[0])
+//        for i in a :
+//            if i in b :
+//                result.append(i)
+//        result.sort()
+//        print(len(result))
+//        for i in result :
+//            print(i)
 
     }
 
